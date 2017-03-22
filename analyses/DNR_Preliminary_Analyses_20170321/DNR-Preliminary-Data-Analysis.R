a04fb9fdb13a4e71c540f036d22c6fc8960677bf
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-03-21 23:26:05 -0700

    started preliminary data analysis

--- analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
+++ analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
@@ -1,12 +1,27 @@
 #In this script, I'll do a preliminary comparison to determine which proteins are differentially expressed between sites and eelgrass conditions.
 
 #Step 1: Import data
-
 averageAreaAdjustedMerged <- read.csv(file = "/Users/yaaminivenkataraman/Documents/School/project-oyster-oa/analyses/DNR_Preliminary_Analyses_20170321/Oyster-AverageAdjustedMergedArea.csv", header = TRUE)
 averageAreaAdjustedMerged <- within(averageAreaAdjustedMerged, rm("X")) #Removing the extra column "X"
 
 #Step 2: Eelgrass vs. Bare
 
-#I didn't do any type of robust statistical analysis (MSstats or otherwise), I just did a few simple pivot tables showing sum area, max area, & average area, then calculated ratios of the above stats with eelgrass/bare and then pulled any protein that had a ratio > 1.5 (more expressed in eelgrass). Again, not robust, but with my limited time and limited data that's good enough for a poster! 
+#Extract columns
+proteins <- averageAreaAdjustedMerged$averageAreaAdjusted.proteins
+
+
+#Find average of all bare and eelgrass sites
+
+#Calculate ratios between eelgrass and bare sites
+ratiosCaseInlet <- averageAreaAdjustedMerged$eelgrassCaseInlet/averageAreaAdjustedMerged$bareCaseInlet
+ratiosFidalgoBay <- averageAreaAdjustedMerged$eelgrassFidalgoBay/averageAreaAdjustedMerged$bareFidalgoBay
+ratiosWillapaBay <- averageAreaAdjustedMerged$eelgrassWillapaBay/averageAreaAdjustedMerged$bareWillapaBay
+ratiosSkokomishRiver <- averageAreaAdjustedMerged$eelgrassSkokomishRiver/averageAreaAdjustedMerged$bareSkokomishRiver
+ratiosPortGamble <- averageAreaAdjustedMerged$eelgrassPortGamble/averageAreaAdjustedMerged$barePortGamble
+
+#Create dataframe with protein names and ratios
+ratiosEelgrassBare <- data.frame(proteins, ratiosCaseInlet, ratiosFidalgoBay, ratiosWillapaBay, ratiosSkokomishRiver, ratiosPortGamble)
+
+#Select rows 
 
 #Step 3: Case Inlet vs. Fidalgo Bay vs. Willapa Bay vs. Skokomish River vs. Port Gamble
