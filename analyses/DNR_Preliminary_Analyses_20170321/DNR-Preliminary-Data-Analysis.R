73cca4c81c522bc0a34552f3bfcc15f9946f4e84
Yaamini <yaamini@D-172-25-149-222.dhcp4.washington.edu>
2017-03-22 09:20:44 -0700

    Average area ratio analysis

--- analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
+++ analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
@@ -1,7 +1,7 @@
 #In this script, I'll do a preliminary comparison to determine which proteins are differentially expressed between sites and eelgrass conditions.
 
 #Step 1: Import data
-averageAreaAdjustedMerged <- read.csv(file = "/Users/yaaminivenkataraman/Documents/School/project-oyster-oa/analyses/DNR_Preliminary_Analyses_20170321/Oyster-AverageAdjustedMergedArea.csv", header = TRUE)
+averageAreaAdjustedMerged <- read.csv(file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Skyline_20170314/Oyster-AverageAdjustedMergedArea.csv", header = TRUE)
 averageAreaAdjustedMerged <- within(averageAreaAdjustedMerged, rm("X")) #Removing the extra column "X"
 
 #Step 2: Eelgrass vs. Bare
@@ -9,15 +9,23 @@ averageAreaAdjustedMerged <- within(averageAreaAdjustedMerged, rm("X")) #Removin
 #Extract columns
 proteins <- averageAreaAdjustedMerged$averageAreaAdjusted.proteins
 
+bareCaseInlet <- averageAreaAdjustedMerged$bareCaseInlet
+bareFidalgoBay <- averageAreaAdjustedMerged$bareFidalgoBay
+bareWillapaBay <- averageAreaAdjustedMerged$bareWillapaBay
+bareSkokomisRiver <- averageAreaAdjustedMerged$bareSkokomishRiver
+barePortGamble <- averageAreaAdjustedMerged$barePortGamble
+
+eelgrassCaseInlet <- averageAreaAdjustedMerged$eelgrassCaseInlet
+eelgrassFidalgoBay <- averageAreaAdjustedMerged$eelgrassFidalgoBay
+eelgrassWillapaBay <- averageAreaAdjustedMerged$eelgrassWillapaBay
+eelgrassSkokomishRiver <- averageAreaAdjustedMerged$eelgrassSkokomishRiver
+eelgrassPortGamble <- averageAreaAdjustedMerged$eelgrassPortGamble
 
 #Find average of all bare and eelgrass sites
+averageBare <- ave(, )
+averageEelgrass
 
 #Calculate ratios between eelgrass and bare sites
-ratiosCaseInlet <- averageAreaAdjustedMerged$eelgrassCaseInlet/averageAreaAdjustedMerged$bareCaseInlet
-ratiosFidalgoBay <- averageAreaAdjustedMerged$eelgrassFidalgoBay/averageAreaAdjustedMerged$bareFidalgoBay
-ratiosWillapaBay <- averageAreaAdjustedMerged$eelgrassWillapaBay/averageAreaAdjustedMerged$bareWillapaBay
-ratiosSkokomishRiver <- averageAreaAdjustedMerged$eelgrassSkokomishRiver/averageAreaAdjustedMerged$bareSkokomishRiver
-ratiosPortGamble <- averageAreaAdjustedMerged$eelgrassPortGamble/averageAreaAdjustedMerged$barePortGamble
 
 #Create dataframe with protein names and ratios
 ratiosEelgrassBare <- data.frame(proteins, ratiosCaseInlet, ratiosFidalgoBay, ratiosWillapaBay, ratiosSkokomishRiver, ratiosPortGamble)
