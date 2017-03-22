bc86793557b3c683afa4ea9b387202cc79d6cb1e
Yaamini <yaamini@D-172-25-149-222.dhcp4.washington.edu>
2017-03-22 09:52:37 -0700

    started NMDS plot

--- analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
+++ analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
@@ -1,35 +1,27 @@
-#In this script, I'll do a preliminary comparison to determine which proteins are differentially expressed between sites and eelgrass conditions.
+#In this script, I'll visualize my preliminary data to see what differences there are between eelgrass and bare patches across sites.
 
 #Step 1: Import data
 averageAreaAdjustedMerged <- read.csv(file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Skyline_20170314/Oyster-AverageAdjustedMergedArea.csv", header = TRUE)
 averageAreaAdjustedMerged <- within(averageAreaAdjustedMerged, rm("X")) #Removing the extra column "X"
 
-#Step 2: Eelgrass vs. Bare
+#Step 2: Nonmetric multidimensional scaling plot (NMDS)
+#Load the source file for the biostats package
+source("biostats.R")
+require(vegan) #Require the vegan package
 
-#Extract columns
-proteins <- averageAreaAdjustedMerged$averageAreaAdjusted.proteins
+#make sure first column of protein names is recognized as row names instead of values
+nsaf.protID2<-nsaf.dat[-1]
+rownames(nsaf.protID2)<-nsaf.dat[,1]
 
-bareCaseInlet <- averageAreaAdjustedMerged$bareCaseInlet
-bareFidalgoBay <- averageAreaAdjustedMerged$bareFidalgoBay
-bareWillapaBay <- averageAreaAdjustedMerged$bareWillapaBay
-bareSkokomisRiver <- averageAreaAdjustedMerged$bareSkokomishRiver
-barePortGamble <- averageAreaAdjustedMerged$barePortGamble
+#transpose the file so that rows and columns are switched and normalized by log(x+1)
+nsaf.t<-t(nsaf.protID2)
+nsaf.tra<-(nsaf.t+1)
+nsaf.tra<-data.trans(nsaf.tra, method='log', plot=F)
 
-eelgrassCaseInlet <- averageAreaAdjustedMerged$eelgrassCaseInlet
-eelgrassFidalgoBay <- averageAreaAdjustedMerged$eelgrassFidalgoBay
-eelgrassWillapaBay <- averageAreaAdjustedMerged$eelgrassWillapaBay
-eelgrassSkokomishRiver <- averageAreaAdjustedMerged$eelgrassSkokomishRiver
-eelgrassPortGamble <- averageAreaAdjustedMerged$eelgrassPortGamble
+#make MDS dissimilarity matrix
+proc.nmds<-metaMDS(nsaf.tra, distance='bray', k=2, trymax=100, autotransform=F)
 
-#Find average of all bare and eelgrass sites
-averageBare <- ave(, )
-averageEelgrass
-
-#Calculate ratios between eelgrass and bare sites
-
-#Create dataframe with protein names and ratios
-ratiosEelgrassBare <- data.frame(proteins, ratiosCaseInlet, ratiosFidalgoBay, ratiosWillapaBay, ratiosSkokomishRiver, ratiosPortGamble)
-
-#Select rows 
-
-#Step 3: Case Inlet vs. Fidalgo Bay vs. Willapa Bay vs. Skokomish River vs. Port Gamble
+#make figure
+fig.nmds<-ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
+points(fig.nmds, 'sites', col=c(whatever colors you want), pch=(whatever points you want))
+legend(x=, y=, pch=(points you chose above), legend=c('legend text 1', 'legend text 2', ...), col=c(colors you chose above))
--- analyses/DNR_Preliminary_Analyses_20170321/DNR-Reformat-Preliminary-Data.R
+++ analyses/DNR_Preliminary_Analyses_20170321/DNR-Reformat-Preliminary-Data.R
@@ -58,3 +58,68 @@ averageAreaAdjustedMerged <- data.frame(averageAreaAdjusted$proteins, bareCaseIn
 #Step 5: Export the new .csv
 
 write.csv(x = averageAreaAdjustedMerged, file = "/Users/yaaminivenkataraman/Documents/School/project-oyster-oa/analyses/DNR_Preliminary_Analyses_20170321/Oyster-AverageAdjustedMergedArea.csv")
+
+
+######
+
+
+#Now, I'll do the same with my maximum area spreadsheet
+
+#Step 1: Import data
+MaxArea <- read.csv(file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Skyline_20170314/Oyster-MaxArea-Proteinbased.csv", header = TRUE)
+MaxArea #View data
+
+#Step 2: Remove columns from poor O107 runs
+maxAreaAdjusted <- within(MaxArea, rm("X2017_January_23_envtstress_oyster2.raw", "X2017_January_23_envtstress_oyster17.raw", "X2017_January_23_envtstress_oyster21.raw", "X2017_January_23_envtstress_oyster22.raw", "X2017_January_23_envtstress_oyster23.raw")) #Based on my sequence file, I need to remove samples oyster 2, 17, 22 and 23. I'm also going to remove the blank.
+maxAreaAdjusted #View data to confirm changes
+
+#Step 3: Change column names
+names(maxAreaAdjusted) <- c("proteins", "bare-willapa-bay-1", "bare-skokomish-river-1", "bare-fidalgo-bay-1", "bare-willapa-bay-2", "eelgrass-fidalgo-bay-1", "bare-port-gamble-1", "bare-case-inlet-1", "bare-skokomish-river-2", "eelgrass-willapa-bay-1", "eelgrass-case-inlet-1", "eelgrass-port-gamble-1", "eelgrass-skokomish-river-1", "eelgrass-skokomish-river-2", "eelgrass-case-inlet-2", "eelgrass-port-gamble-2", "bare-fidalgo-bay-2", "bare-port-gamble-2", "eelgrass-fidalgo-bay-2", "bare-case-inlet-2", "eelgrass-willapa-bay-2") #Change the column names to indicate condition, location, and replicate
+maxAreaAdjusted #View data to ensure the column names changed
+
+#Step 4: Merge columns together. I want to average the values between my replicates so I have a total of 10 columns with data to analyze.
+
+#First, I need to extract all of the columns
+bareCaseInlet1 <- maxAreaAdjusted$`bare-case-inlet-1`
+bareCaseInlet2 <- maxAreaAdjusted$`bare-case-inlet-2`
+bareFidalgoBay1 <- maxAreaAdjusted$`bare-fidalgo-bay-1`
+bareFidalgoBay2 <- maxAreaAdjusted$`bare-fidalgo-bay-1`
+bareWillapaBay1 <- maxAreaAdjusted$`bare-willapa-bay-1`
+bareWillapaBay2 <- maxAreaAdjusted$`bare-willapa-bay-2`
+bareSkokomishRiver1 <- maxAreaAdjusted$`bare-skokomish-river-1`
+bareSkokomishRiver2 <- maxAreaAdjusted$`bare-skokomish-river-2`
+barePortGamble1 <- maxAreaAdjusted$`bare-port-gamble-1`
+barePortGamble2 <- maxAreaAdjusted$`bare-port-gamble-2`
+
+eelgrassCaseInlet1 <- maxAreaAdjusted$`eelgrass-case-inlet-1`
+eelgrassCaseInlet2 <- maxAreaAdjusted$`eelgrass-case-inlet-2`
+eelgrassFidalgoBay1 <- maxAreaAdjusted$`eelgrass-fidalgo-bay-1`
+eelgrassFidalgoBay2 <- maxAreaAdjusted$`eelgrass-fidalgo-bay-2`
+eelgrassWillapaBay1 <- maxAreaAdjusted$`eelgrass-willapa-bay-1`
+eelgrassWillapaBay2 <- maxAreaAdjusted$`eelgrass-willapa-bay-2`
+eelgrassSkokomishRiver1 <- maxAreaAdjusted$`eelgrass-skokomish-river-1`
+eelgrassSkokomishRiver2 <- maxAreaAdjusted$`eelgrass-skokomish-river-2`
+eelgrassPortGamble1 <- maxAreaAdjusted$`eelgrass-port-gamble-1`
+eelgrassPortGamble2 <- maxAreaAdjusted$`eelgrass-port-gamble-2`
+
+#Then I will average all of the pairs
+
+bareCaseInlet <- ave(bareCaseInlet1, bareCaseInlet2)
+bareFidalgoBay <- ave(bareFidalgoBay1, bareFidalgoBay2)
+bareWillapaBay <- ave(bareWillapaBay1, bareWillapaBay2)
+bareSkokomishRiver <- ave(bareSkokomishRiver1, bareSkokomishRiver2)
+barePortGamble <- ave(barePortGamble1, barePortGamble2)
+
+eelgrassCaseInlet <- ave(eelgrassCaseInlet1, eelgrassCaseInlet2)
+eelgrassFidalgoBay <- ave(eelgrassFidalgoBay1, eelgrassFidalgoBay2)
+eelgrassWillapaBay <- ave(eelgrassWillapaBay1, eelgrassWillapaBay2)
+eelgrassSkokomishRiver <- ave(eelgrassSkokomishRiver1, eelgrassSkokomishRiver2)
+eelgrassPortGamble <- ave(eelgrassPortGamble1, eelgrassPortGamble2)
+
+#And finally I'll create a new data frame
+
+maxAreaAdjustedMerged <- data.frame(maxAreaAdjusted$proteins, bareCaseInlet, bareFidalgoBay, bareWillapaBay, bareSkokomishRiver, barePortGamble, eelgrassCaseInlet, eelgrassFidalgoBay, eelgrassWillapaBay, eelgrassSkokomishRiver, eelgrassPortGamble)
+
+#Step 5: Export the new .csv
+
+write.csv(x = maxAreaAdjustedMerged, file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Preliminary_Analyses_20170321/Oyster-MaxAdjustedMergedArea.csv")
