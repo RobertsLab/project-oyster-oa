02b869854af283af2b1e9e9223dbe9879adc5122
Yaamini <yaamini@D-172-25-149-222.dhcp4.washington.edu>
2017-03-22 11:13:59 -0700

    Created prelim NMDS plot

--- analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
+++ analyses/DNR_Preliminary_Analyses_20170321/DNR-Preliminary-Data-Analysis.R
@@ -1,27 +1,30 @@
 #In this script, I'll visualize my preliminary data to see what differences there are between eelgrass and bare patches across sites.
 
 #Step 1: Import data
-averageAreaAdjustedMerged <- read.csv(file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Skyline_20170314/Oyster-AverageAdjustedMergedArea.csv", header = TRUE)
+averageAreaAdjustedMerged <- read.csv(file = "/Users/yaamini/Documents/project-oyster-oa/analyses/DNR_Skyline_20170314/Oyster-AverageAdjustedMergedArea.csv", header = TRUE, na.strings = "NA")
 averageAreaAdjustedMerged <- within(averageAreaAdjustedMerged, rm("X")) #Removing the extra column "X"
+averageAreaAdjustedMerged[is.na(averageAreaAdjustedMerged)] <- 0 #Replace NAs with 0s
+averageAreaAdjustedMerged #Confirm changes
 
 #Step 2: Nonmetric multidimensional scaling plot (NMDS)
 #Load the source file for the biostats package
 source("biostats.R")
-require(vegan) #Require the vegan package
+install.packages("vegan") #Install vegan package
+library(vegan)
 
-#make sure first column of protein names is recognized as row names instead of values
-nsaf.protID2<-nsaf.dat[-1]
-rownames(nsaf.protID2)<-nsaf.dat[,1]
+#Make sure first column of protein names is recognized as row names instead of values
+area.protID2 <- averageAreaAdjustedMerged[-1]
+rownames(area.protID2) <- averageAreaAdjustedMerged[,1]
 
-#transpose the file so that rows and columns are switched and normalized by log(x+1)
-nsaf.t<-t(nsaf.protID2)
-nsaf.tra<-(nsaf.t+1)
-nsaf.tra<-data.trans(nsaf.tra, method='log', plot=F)
+#Transpose the file so that rows and columns are switched and normalized by log(x+1)
+area.t <- t(area.protID2)
+area.tra <- (area.t+1)
+area.tra <- data.trans(area.tra, method = 'log', plot = FALSE)
 
-#make MDS dissimilarity matrix
-proc.nmds<-metaMDS(nsaf.tra, distance='bray', k=2, trymax=100, autotransform=F)
+#Make MDS dissimilarity matrix
+proc.nmds <- metaMDS(area.tra, distance = 'bray', k = 2, trymax = 100, autotransform = FALSE)
 
-#make figure
-fig.nmds<-ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
-points(fig.nmds, 'sites', col=c(whatever colors you want), pch=(whatever points you want))
-legend(x=, y=, pch=(points you chose above), legend=c('legend text 1', 'legend text 2', ...), col=c(colors you chose above))
+#Make figure
+fig.nmds <- ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
+points(fig.nmds, 'sites', col=c(1), pch=(19))
+legend(x=, y=, pch=(19), legend=c('legend text 1', 'legend text 2', ...), col=c(1))
