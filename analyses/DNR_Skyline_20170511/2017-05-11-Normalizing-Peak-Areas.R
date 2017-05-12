2d9ab38c96ef3360f095bce72c8bc34d4299d7fe
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-05-11 20:47:11 -0700

    changed column names to reflect DNR vial, sample site and eelgrass condition

--- analyses/DNR_Skyline_20170511/2017-05-11-Normalizing-Peak-Areas.R
+++ analyses/DNR_Skyline_20170511/2017-05-11-Normalizing-Peak-Areas.R
@@ -31,5 +31,11 @@ peakAreas[,22] <- peakAreas$X1.Area/TICvalues$oyster[19]
 peakAreas[,23] <- peakAreas$X1.Area/TICvalues$oyster[20]
 peakAreas[,24] <- peakAreas$X1.Area/TICvalues$oyster[21]
 
+#Rename column headers to include DNR vial number, eelgrass condition and sample site.
+
+names(peakAreas) <- c("protein-name", "peptide", "peptide-sequence", "O127-wb-bare-1",
+"O107-sk-eelgrass-1", "O07-ci-eelgrass-1", "O77-pg-eelgrass-1", "O47-fb-bare-1", "O55-pg-bare-1", "O37-fb-eelgrass-1", "O15-ci-eelgrass-1", "O142-wb-eelgrass-1", "O119-sk-bare-1", "O47-fb-bare-2", "O127-wb-bare-2", "O37-fb-eelgrass-2", "O55-pg-bare-2", "O15-ci-bare-2", "O119-sk-bare-2", "O142-wb-eelgrass-2", "O07-ci-eelgrass-2", "O77-pg-eelgrass-2", "O107-2-sk-eegrass-1", "O107-2-sk-eelgrass-2")
+head(peakAreas) #Confirm changes
+
 #Save peakAreas as a new CSV file
-write.csv(x = peakAreas, file = "2017-05-11-normalized-peak-areas")
+write.csv(x = peakAreas, file = "2017-05-11-normalized-peak-areas.csv")
