571476e330dae87d8a445cbb438bcacd6fb8361d
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-05-18 10:57:16 -0700

    Have duplicate rows, need to remove

--- analyses/DNR_Skyline_20170512/MSstats/2017-05-13-MSstats.R
+++ analyses/DNR_Skyline_20170512/MSstats/2017-05-13-MSstats.R
@@ -1,3 +1,11 @@
+#### INSTALL MSSTATS ####
+
 source("http://bioconductor.org/biocLite.R")
 biocLite("MSstats")
 library(MSstats)
+
+#### IMPORT AND PROCESS DATA ####
+peakAreas <- read.csv("2017-05-13-peptide-results-MSstats.csv", na.strings = "#N/A") #Import peak area data
+names(peakAreas) <- c("ProteinName", "PeptideSequence", "PrecursorCharge", "FragmentIon", "ProductCharge", "IsotopeLabelType", "Condition", "Run", "BioReplicate", "Intensity") #Rename columns, primarily to change "FileName" and "Area" to "Run" and "Intensity"
+head(peakAreas) #Confirm changes
+QuantData <- dataProcess(peakAreas) #Process data using default settings of log2 transformation and constant normalization based on reference signals (equalize means)
