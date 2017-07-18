0eced273df0da0db4245acc9641525a08d140e36
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-07-18 10:02:25 -0700

    wrote out final transition proteins, pvalues and goterms for REVIGO

--- analyses/DNR_TransitionSelection_20170707/2017-07-08-Final-Transitions/2017-07-18-Final-Transition-Visualizations.R
+++ analyses/DNR_TransitionSelection_20170707/2017-07-08-Final-Transitions/2017-07-18-Final-Transition-Visualizations.R
@@ -1,11 +1,11 @@
-#### IMPORT DATA ####
+#### IMPORT FULL PROTEIN AREA DATA ####
 
 proteinAreas <- read.csv("2017-06-10-protein-areas-only-error-checked.csv", na.strings = "#N/A") #Specify Skyline's special way of designating N/A values
 head(proteinAreas) #Confirm successful import
 averageProteinAreas <- aggregate(proteinAreas[-1], proteinAreas[1], mean, na.action = na.omit, na.rm = TRUE) #Average protein areas across transitions and peptides
 head(averageProteinAreas) #Confirm changes 
 
-#### REFORMAT DATA ####
+#### REFORMAT FULL PROTEIN DATA ####
 
 names(averageProteinAreas) <- c("protein", "wb-bare-1", "sk-eelgrass-1", "ci-eelgrass-1", "pg-eelgrass-1", "fb-bare-1", "pg-bare-1", "fb-eelgrass-1", "ci-bare-1", "wb-eelgrass-1", "sk-bare-1", "fb-bare-2", "wb-bare-2", "fb-eelgrass-2", "pg-bare-2", "ci-bare-2", "sk-bare-2", "wb-eelgrass-2", "ci-eelgrass-2", "pg-eelgrass-2", "sk-eelgrass-2", "sk-eelgrass-3")
 head(averageProteinAreas) #Confirm changes
@@ -24,12 +24,34 @@ eelgrassWillapaBay <- ave(averageProteinAreas$`wb-eelgrass-1`, averageProteinAre
 eelgrassSkokomishRiver <- ave(averageProteinAreas$`sk-eelgrass-2`, averageProteinAreas$`sk-eelgrass-3`)
 eelgrassPortGamble <- ave(averageProteinAreas$`pg-eelgrass-1`, averageProteinAreas$`pg-eelgrass-2`)
 
-averageProteinAreasMerged <- data.frame(averageProteinAreas$protein, bareCaseInlet, bareFidalgoBay, barePortGamble, bareSkokomishRiver, bareWillapaBay, eelgrassCaseInlet, eelgrassFidalgoBay, eelgrassPortGamble, eelgrassSkokomishRiver, eelgrassWillapaBay)
-
-#### FULL NMDS PLOT ####
-
-averageProteinAreasMerged[is.na(averageProteinAreasMerged)] <- 0 #Replace NAs with 0s
+averageProteinAreasMerged <- data.frame(averageProteinAreas$protein, bareCaseInlet, bareFidalgoBay, barePortGamble, bareSkokomishRiver, bareWillapaBay, eelgrassCaseInlet, eelgrassFidalgoBay, eelgrassPortGamble, eelgrassSkokomishRiver, eelgrassWillapaBay) #Create new dataframe
+names(averageProteinAreasMerged) <- c("protein", "bareCaseInlet", "bareFidalgoBay", "barePortGamble", "bareSkokomishRiver", "bareWillapaBay", "eelgrassCaseInlet", "eelgrassFidalgoBay", "eelgrassPortGamble", "eelgrassSkokomishRiver", "eelgrassWillapaBay") #Rename columns
 head(averageProteinAreasMerged) #Confirm changes
+
+#### ISOLATE PROTEINS FROM FINAL TRANSITION LIST ####
+
+transitionList <- read.csv("2017-07-10-SRM-Transitions-With-PRTC.csv", header = FALSE) #Import final transition list
+names(transitionList) <- c("V1", "V2", "retentionTime", "peptide", "protein", "V6", "V7") #Rename columns
+transitionListTargetsOnly <- transitionList[-(124:153),] #Remove PRTC peptides
+tail(transitionListTargetsOnly) #Confirm removal
+transitionProteins <- transitionListTargetsOnly[,c(4,5)] #Since I want to merge by protein names to examine differential expression across sample sites, I don't need the transitions or peptides. I just need protein names and peak area. Therefore, I removed everything from the final transition list except for the column with protein names and peptides
+head(transitionProteins) #Confirm changes
+uniqueTransitionProteins <- unique(transitionProteins) #Removing duplicate entries from transitionProteins
+head(uniqueTransitionProteins) #Confirm changes...didn't work. Will try again post-merge
+
+#### MERGE FULL PROTEIN DATA WITH FINAL TRANSITION PROTEINS ####
+
+uniqueTransitionProteinAreas <- merge(uniqueTransitionProteins, averageProteinAreasMerged, by = "protein")
+head(uniqueTransitionProteinAreas) #Confirm merge
+uniqueTransitionProteinAreas <- uniqueTransitionProteinAreas[,-2] #Remove peptide column
+head(uniqueTransitionProteinAreas) #Confirm changes
+uniqueTransitionProteinAreas <- unique(uniqueTransitionProteinAreas) #Remove duplicate rows.
+head(uniqueTransitionProteinAreas) #Confirm changes. It works!
+
+#### NMDS PLOT ####
+
+uniqueTransitionProteinAreas[is.na(uniqueTransitionProteinAreas)] <- 0 #Replace NAs with 0s
+head(uniqueTransitionProteinAreas) #Confirm changes
 
 #Load the source file for the biostats package
 source("biostats.R") #Either load the source R script or copy paste. Must run this code before NMDS.
@@ -37,8 +59,8 @@ install.packages("vegan") #Install vegan package
 library(vegan)
 
 #Make sure first column of protein names is recognized as row names instead of values
-area.protID2 <- averageProteinAreasMerged[-1]
-rownames(area.protID2) <- averageProteinAreasMerged[,1]
+area.protID2 <- uniqueTransitionProteinAreas[-1]
+rownames(area.protID2) <- uniqueTransitionProteinAreas[,1]
 
 #Transpose the file so that rows and columns are switched and normalized by log(x+1)
 area2.t <- t(area.protID2[,1:10])
@@ -58,8 +80,8 @@ fig.nmds <- ordiplot(proc.nmds, choices=c(1,2), type='none', display='sites', xl
 #skokomish=green
 #gamble=magenta
 
-points(fig.nmds, 'sites', col=c('red', 'blue', 'black', 'green', 'magenta','red', 'blue', 'black', 'green', 'magenta'), pch=c(rep(16,5), rep(17,5)))
-legend(-0.045,0.025, pch=c(rep(16,5), 1, 2), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble", "Bare", "Eelgrass"), col=c('red', 'blue', 'black', 'green', 'magenta', 'black', 'black'))
+points(fig.nmds, 'sites', col=c('red', 'blue', 'black', 'green', 'magenta','red', 'blue', 'black', 'green', 'magenta'), pch=c(rep(16,5), rep(17,5))) #Looks like all of the points are on top of eachother.......
+legend(0,0.06, pch=c(rep(16,5), 1, 2), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble", "Bare", "Eelgrass"), col=c('red', 'blue', 'black', 'green', 'magenta', 'black', 'black')) 
 
 #### FULL HEATMAP ####
 
@@ -72,30 +94,18 @@ library(pheatmap)
 #Create heatmap
 pheatmap(area2.tra, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'average', show_rownames = T, show_colnames = F)
 
-#Export preliminary heatmap as a .png
-png(filename = "fullHeatmap.png")
+#Export preliminary heatmap as a .jpeg
+jpeg(filename = "transitionHeatmap.jpeg")
 pheatmap(area2.tra, cluster_rows = T, cluster_cols = T, clustering_distance_rows = 'euclidean', clustering_distance_cols = 'euclidean', clustering_method = 'average', show_rownames = T, show_colnames = F)
 dev.off()
 
-#### MERGE WITH GO TERMS ####
-
-#Upload table with proteins and accession codes.
-proteinAccessionCodes <- read.table(file = "background-proteome-accession.txt", header = FALSE, col.names = c("averageAreaAdjusted.proteins", "accession", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12"))
-proteinAccessionCodes <- within(proteinAccessionCodes, rm("V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "V11", "V12")) #Removing the extra columns
-names(proteinAccessionCodes) <- c("averageProteinAreas.protein", "accession")
-head(proteinAccessionCodes) #View uploaded data and confirm changes
-head(averageProteinAreasMerged)
-
-#Merge Skyline output with Uniprot information
-skylineProteinAccession <- merge(x = averageProteinAreasMerged, y = proteinAccessionCodes, by = "averageProteinAreas.protein")
-head(skylineProteinAccession) #confirm merge
-
-#### WRITE OUT MERGED TABLE ####
-
-#Write out alltreatments_DEG_Uniprot as a tab file. Remove row and column names using "row.names" and "col.names" arguments
-write.table(skylineProteinAccession, "2017-06-13-Skyline-ProteinAccession-nohead.txt", col.names = F, row.names = F)
-
-#### MERGE WITH ANNOTATIONS ####
-proteinAnnotationsEvalues <- read.csv(file = "/Users/yaaminivenkataraman/Documents/project-oyster-oa/analyses/DNR_TransitionSelection_20170707/2017-07-07-Preliminary-Transitions/2017-07-07-Gigas-Annotations-Evalues.csv", header = FALSE, col.names = c("C1", "averageProteinAreas.protein", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "Evalue", "C14", "C15", "reviewed", "Annotation", "C18", "Species", "C20", "BiologicalProcess", "GOTerm", "Pathway", "C24", "C25", "C26")) #Import full annotation
-fullSkylineAnnotations <- merge(x = averageProteinAreasMerged, y = proteinAnnotationsEvalues, by = "averageProteinAreas.protein")
-write.csv(fullSkylineAnnotations, "2017-07-09-Full-Skyline-Output-Annotations.csv")
+#### MERGE WITH GOTERMS AND PVALUES ####
+sitesAndEelgrass <- read.csv(file = "/Users/yaaminivenkataraman/Documents/project-oyster-oa/analyses/DNR_TransitionSelection_20170707/2017-07-07-Preliminary-Transitions/2017-07-05-SitesEelgrass-Accession-nohead.csv")
+head(sitesAndEelgrass)
+goTermspValues <- sitesAndEelgrass[,c(1, 8, 18)] #Isolate GOterms and pvalues
+head(goTermspValues) #Confirm isolation
+names(goTermspValues) <- c("protein", "adj.pvalue", "goterm") #Rename columns
+head(goTermspValues) #Confirm changes
+uniqueTransitionProteinAreasGO <- merge(x = uniqueTransitionProteinAreas, y = goTermspValues, by = "protein")
+head(uniqueTransitionProteinAreasGO) #This list gives me the p-values for each replicate. I can sort through this manually before I use the information in REVIGO.
+write.csv(x = uniqueTransitionProteinAreasGO, file = "2017-07-18-Transition-Proteins-for-REVIGO.csv")
