683a8c42e687f23788fa0b5470526ee217f5150d
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-09-13 16:37:30 -0700

    made boxplots!

--- analyses/DNR_SRM_20170902/2017-09-12-Protein-Area-Boxplots.R
+++ analyses/DNR_SRM_20170902/2017-09-12-Protein-Area-Boxplots.R
@@ -1,6 +1,7 @@
 #Before going through this script, I went through "2017-09-10-NMDS-ANOSIM-for-Cluster-Analysis." In this script, I'll depict normalized protein area across samples as bar charts
 
 #### DATA MANIPULATION ####
+
 SRMDataNMDSAveragedCorrected #From 2017-09-06-NMDS-for-Technical-Replication. Average normalized area data.
 boxplotData <- data.frame(t(SRMDataNMDSAveragedCorrected)) #Transpose the data
 boxplotData$Sample.Number <- rownames(boxplotData) #Save rownames as a new column
@@ -42,16 +43,3 @@ for(i in 3:nTransitions) { #For all of my columns with transition IDs
   boxplot(boxplotData[,i] ~ boxplotData$Site + boxplotData$Eelgrass.Condition, xlab = "Sites", ylab = "Abundance", names = c("CI.Bare", "FB.Bare", "PG.Bare", "SK.Bare", "WB.Bare", "CI.Eelgrass", "FB.Eelgrass", "PG.Eelgrass", "SK.Eelgrass", "WB.Eelgrass")) #Create the boxplot
   dev.off() #Close file
 }
-
- #Create the boxplot
-?boxplot
-
-
-
-
-
-
-
-
-
-
