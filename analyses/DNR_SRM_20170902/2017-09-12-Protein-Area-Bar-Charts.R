087dd1e2631d006b5ce6c3fc633f113110db85cc
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-09-13 09:44:50 -0700

    draft catalase bar chart

--- analyses/DNR_SRM_20170902/2017-09-12-Protein-Area-Bar-Charts.R
+++ analyses/DNR_SRM_20170902/2017-09-12-Protein-Area-Bar-Charts.R
@@ -2,13 +2,15 @@
 
 #### BASELINE DATA MANIPULATION ####
 
-SRMDataBarplots <- masterSRMDataBiologicalReplicates #Duplicate dataframe
+SRMDataBarplots <- read.csv("2017-09-07-Master-SRM-Data-BiologicalReplicates-NoBlanks-NoPivot.csv", na.strings = "N/A") #Read in data
 head(SRMDataBarplots)
-SRMDataBarplots <- SRMDataBarplots[,-c(2, 5, 7)] #Remove extraneous columns
+SRMDataBarplots <- SRMDataBarplots[,-c(1, 3, 6, 8)] #Remove extraneous columns
+SRMDataBarplots$Area <- as.numeric(SRMDataBarplots$Area)
+SRMDataBarplots$TIC <- as.numeric(SRMDataBarplots$TIC)
 SRMDataBarplots$Normalized.Area <- (SRMDataBarplots$Area)/(SRMDataBarplots$TIC) #Normalize area
 SRMDataBarplots$Protein.Peptide.Transition <- paste(SRMDataBarplots$Protein.Name, SRMDataBarplots$Peptide.Sequence, SRMDataBarplots$Fragment.Ion) #Combine protein IDs
 head(SRMDataBarplots)
-SRMDataBarplots <- SRMDataBarplots[,-c(3:5)] #Remove extraneous columns, but keep protein name
+SRMDataBarplots <- SRMDataBarplots[,-c(3:6)] #Remove extraneous columns, but keep protein name
 head(SRMDataBarplots)
 attach(SRMDataBarplots)
 SRMDataBarplots <- SRMDataBarplots[order(Protein.Name),] #Reorder by protein name
@@ -16,10 +18,6 @@ head(SRMDataBarplots)
 detach(SRMDataBarplots)
 
 #### BAR CHARTS WITHOUT AVERAGING ACROSS SITES AND CONDITIONS ####
-
-
-
-barplot(SRMDataNMDSAveragedCorrected$`O01-1`)
 
 #### BAR CHARTS AFTER AVERAGING ACROSS UNIQUE SITE + CONDITION COMBINATIONS ####
 
@@ -59,4 +57,5 @@ willapaCatalaseP1T1 <- mean(willapaCatalase$Normalized.Area[1:18])
 catalaseP1T1 <- data.frame(Site = c("Case Inlet", "Fidalgo Bay", "Port Gamble Bay", "Skokomish River", "Willapa Bay"),
                            Area = c(caseCatalaseP1T1, fidalgoCatalaseP1T1, portCatalaseP1T1, skokomishCatalaseP1T1, willapaCatalaseP1T1))
 
-plot(catalaseP1T1, type = "p")
+barplot(catalaseP1T1$Area, xlab = "Sites", ylab = "Abundance", main = "Catalase Abundance across Sites", col = c("red", "blue", "black", "green", "magenta"), legend = TRUE)
+legend("topleft", cex = .5, pch = c(rep(x = 16, times = 5)), legend=c('Case Inlet', "Fidalgo Bay", "Port Gamble", "Skokomish River", "Willapa Bay"), col=c('red', 'blue', 'black', 'green', 'magenta'))
