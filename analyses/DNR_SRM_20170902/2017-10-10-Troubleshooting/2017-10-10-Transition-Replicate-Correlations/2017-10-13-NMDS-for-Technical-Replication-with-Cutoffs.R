50a950ae48e1630683669d9b59f94267b1a4c9ea
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-10-13 17:59:07 -0700

    repeated analysis with a 0.7 cutoff and nonnormalized data

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-10-Transition-Replicate-Correlations/2017-10-13-NMDS-for-Technical-Replication-with-Cutoffs.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-10-Transition-Replicate-Correlations/2017-10-13-NMDS-for-Technical-Replication-with-Cutoffs.R
@@ -1,4 +1,4 @@
-#In this script, I'll use two different adjusted R-squared cutoffs to screen transition data and reassess my technical replication. I'll try these cutoffs both with and without normalizing my data.
+#In this script, I'll use three different adjusted R-squared cutoffs to screen transition data and reassess my technical replication. I'll try these cutoffs both with and without normalizing my data.
 
 #### CUTOFF = 0.6, NONNORMALIZED DATA ####
 
@@ -68,7 +68,8 @@ for(i in 1:nSamples) { #For rows in NMDSCoordinatesNonNormalizedCutoff1
   sampleDistancesNonNormalizedCutoff1[i] <- sqrt((NMDSCoordinatesNonNormalizedCutoff1[i,1]-NMDSCoordinatesNonNormalizedCutoff1[i,2])^2 + (NMDSCoordinatesNonNormalizedCutoff1[i+1,1]-NMDSCoordinatesNonNormalizedCutoff1[i+1,2])^2) #Calculate distance between ordinations
   print(sampleDistancesNonNormalizedCutoff1[i]) #Print the distance value
 }
-sampleDistancesNonNormalizedCutoff1 #Confirm vector creation. This vector has all consecutive pairs, including those that are not paris of technical replicates. I need to retain just the odd numbered rows.
+
+sampleDistancesNonNormalizedCutoff1 #Confirm vector creation. This vector has all consecutive pairs, including those that are not pairs of technical replicates. I need to retain just the odd numbered rows.
 technicalReplicatesNonNormalizedCuttof1 <- rownames(NMDSCoordinatesNonNormalizedCutoff1) #Save rownames as a new vector
 technicalReplicatesNonNormalizedCuttof1 #Confirm vector creation
 technicalReplicateDistancesNonNormalizedCuttof1 <- data.frame(Sample = technicalReplicatesNonNormalizedCuttof1[seq(from = 1, to = nSamples, by = 2)], 
@@ -163,6 +164,97 @@ tail(technicalReplicateDistancesNormalizedCuttof1) #Confirm dataframe creation
 plot(x = technicalReplicateDistancesNormalizedCuttof1$Sample, y = technicalReplicateDistancesNormalizedCuttof1$Distance, type = "line", xlab = "Sample", ylab = "Distance between Ordinations")
 #dev.off()
 
+
+#### CUTOFF = 0.7, NONNORMALIZED DATA ####
+
+#Remove transitions with R-squared values below 0.7 cutoff. Based on analyses in 2017-10-10-Correlating-Transitions-in-Technical-Replicates.R, the following rows (individual transitions) should be removed: 1, 2, 3, 4, 5, 6, 18, 19, 21, 22, 26, 28, 30, 43, 47, 55, 67, 68, 69, 70, 76, 77, 79, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 99, 103, 106, 107, 108, 109, 110, 111
+
+SRMDataNMDSNonNormalizedPivotedCutoff2 <- SRMDataNMDSNonNormalizedPivoted #Duplicate dataframe
+SRMDataNMDSNonNormalizedPivotedCutoff2 <- SRMDataNMDSNonNormalizedPivotedCutoff2[-c(1, 2, 3, 4, 5, 6, 18, 19, 21, 22, 26, 28, 30, 43, 47, 55, 67, 68, 69, 70, 76, 77, 79, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 99, 103, 106, 107, 108, 109, 110, 111),] #Remove rows that don't make the cutoff
+rownames(SRMDataNMDSNonNormalizedPivotedCutoff2) #See if any peptide has less than 2 transitions, and if any protein has less than 2 peptides remaining
+SRMDataNMDSNonNormalizedPivotedCutoff2 <- SRMDataNMDSNonNormalizedPivotedCutoff2[-c(1:3, 12, 15:17, 48:50, 56, 57:65),] #Remove peptides and proteins that don't fit the above criteria. I cut more than I expected.
+rownames(SRMDataNMDSNonNormalizedPivotedCutoff2) #Confirm changes
+
+#Load the source file for the biostats package
+#source("biostats.R") #Either load the source R script or copy paste
+#install.packages("vegan") #Install vegan package
+#library(vegan)
+
+#Format data for NMDS
+SRMDataNMDSNonNormalizedPivotedCorrectedCutoff2 <- SRMDataNMDSNonNormalizedPivotedCutoff2 #Duplicate dataframe
+SRMDataNMDSNonNormalizedPivotedCorrectedCutoff2[is.na(SRMDataNMDSNonNormalizedPivotedCorrectedCutoff2)] <- 0 #Replace NAs with 0s
+head(SRMDataNMDSNonNormalizedPivotedCorrectedCutoff2) #Confirm there are no NAs
+
+area.t3 <- t(SRMDataNMDSNonNormalizedPivotedCorrectedCutoff2) #Transpose the file so that rows and columns are switched
+head(area.t3) #Confirm transposition
+area.tra3 <- (area.t3+1) #Add 1 to all values before transforming
+area.tra3 <- data.trans(area.tra3, method = 'log', plot = FALSE) #log(x+1) transformation
+
+#Rerun NMDS
+proc.nmds.nonnorm.cutoff2.euclidean <- metaMDS(area.t3, distance = 'euclidean', k = 2, trymax = 10000, autotransform = FALSE) #Make MDS dissimilarity matrix using euclidean distance.
+stressplot(proc.nmds.nonnorm.cutoff2.euclidean) #Make Shepard plot
+#ordiplot(proc.nmds.nonnorm.cutoff2.euclidean) #Plot basic NMDS
+#vec.proc.nmds.nonnorm.cutoff2.euclidean <- envfit(proc.nmds.nonnorm.cutoff2.euclidean$points, area.t, perm = 1000) #Calculate loadings
+ordiplot(proc.nmds.nonnorm.cutoff2.euclidean, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
+#plot(vec.proc.nmds.nonnorm.cutoff2.euclidean, p.max=.01, col='blue') #Plot eigenvectors
+
+jpeg(filename = "2017-10-10-Troubleshooting/2017-10-10-Transition-Replicate-Correlations/2017-10-13-NMDS-TechnicalReplication-NonNormalized-Cutoff2.jpeg", width = 1000, height = 1000)
+ordiplot(proc.nmds.nonnorm.cutoff2.euclidean, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
+dev.off()
+
+#Calculate distance between ordinations
+NMDSCoordinatesNonNormalizedCutoff2 <- proc.nmds.nonnorm.cutoff2.euclidean$points #Save NMDS coordinates of each point in a new dataframe
+head(NMDSCoordinatesNonNormalizedCutoff2) #Confirm dataframe creation
+nSamples <- length(NMDSCoordinatesNonNormalizedCutoff2)/2 #Calculate the number of samples
+sampleDistancesNonNormalizedCutoff2 <- vector(length = nSamples) #Create an empty vector to store distance values
+
+for(i in 1:nSamples) { #For rows in NMDSCoordinatesNonNormalizedCutoff1
+  sampleDistancesNonNormalizedCutoff2[i] <- sqrt((NMDSCoordinatesNonNormalizedCutoff2[i,1]-NMDSCoordinatesNonNormalizedCutoff2[i,2])^2 + (NMDSCoordinatesNonNormalizedCutoff2[i+1,1]-NMDSCoordinatesNonNormalizedCutoff2[i+1,2])^2) #Calculate distance between ordinations
+  print(sampleDistancesNonNormalizedCutoff2[i]) #Print the distance value
+}
+
+sampleDistancesNonNormalizedCutoff2 #Confirm vector creation. This vector has all consecutive pairs, including those that are not pairs of technical replicates. I need to retain just the odd numbered rows.
+technicalReplicatesNonNormalizedCuttof2 <- rownames(NMDSCoordinatesNonNormalizedCutoff2) #Save rownames as a new vector
+technicalReplicatesNonNormalizedCuttof2 #Confirm vector creation
+technicalReplicateDistancesNonNormalizedCuttof2 <- data.frame(Sample = technicalReplicatesNonNormalizedCuttof2[seq(from = 1, to = nSamples, by = 2)], 
+                                                              Distance = sampleDistancesNonNormalizedCutoff2[seq(from = 1, to = nSamples, by = 2)]) #Create a new dataframe with just odd numbered row distances (technical replicate pairs)
+head(technicalReplicateDistancesNonNormalizedCuttof2) #Confirm dataframe creation
+tail(technicalReplicateDistancesNonNormalizedCuttof2) #Confirm dataframe creation
+
+#Plot distance between technical replicate ordinations and save
+#jpeg(filename = "2017-10-10-Troubleshooting/2017-10-10-Transition-Replicate-Correlations/2017-10-13-NMDS-TechnicalReplication-Ordination-Distances-NonNormalized-Cutoff2.jpeg", width = 1000, height = 1000)
+plot(x = technicalReplicateDistancesNonNormalizedCuttof2$Sample, y = technicalReplicateDistancesNonNormalizedCuttof2$Distance, type = "line", xlab = "Sample", ylab = "Distance between Ordinations")
+#dev.off()
+
+#### CUTOFF = 0.7, NORMALIZED DATA ####
+
+#Remove transitions with R-squared values below 0.7 cutoff. Based on analyses in 2017-10-10-Correlating-Transitions-in-Technical-Replicates.R, the following rows (individual transitions) should be removed: 1, 2, 3, 4, 5, 6, 18, 19, 21, 22, 26, 28, 30, 43, 47, 55, 67, 68, 69, 70, 76, 77, 79, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 99, 103, 106, 107, 108, 109, 110, 111
+
+SRMDataNMDSNormalizedPivotedCutoff2 <- SRMDataNMDSNormalizedPivoted #Duplicate dataframe
+SRMDataNMDSNormalizedPivotedCutoff2 <- SRMDataNMDSNormalizedPivotedCutoff2[-c(1, 2, 3, 4, 5, 6, 18, 19, 21, 22, 26, 28, 30, 43, 47, 55, 67, 68, 69, 70, 76, 77, 79, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 96, 97, 98, 99, 103, 106, 107, 108, 109, 110, 111),] #Remove rows that don't make the cutoff
+rownames(SRMDataNMDSNormalizedPivotedCutoff2) #See if any peptide has less than 2 transitions, and if any protein has less than 2 peptides remaining
+SRMDataNMDSNormalizedPivotedCutoff2 <- SRMDataNMDSNormalizedPivotedCutoff2[-c(1:3, 12, 15:17, 48:50, 56, 57:65),] #Remove peptides and proteins that don't fit the above criteria. I cut more than I expected.
+rownames(SRMDataNMDSNormalizedPivotedCutoff2) #Confirm changes
+
+#Load the source file for the biostats package
+#source("biostats.R") #Either load the source R script or copy paste
+#install.packages("vegan") #Install vegan package
+#library(vegan)
+
+#Format data for NMDS
+SRMDataNMDSNormalizedPivotedCorrectedCutoff2 <- SRMDataNMDSNormalizedPivotedCutoff2 #Duplicate dataframe
+SRMDataNMDSNormalizedPivotedCorrectedCutoff2[is.na(SRMDataNMDSNormalizedPivotedCorrectedCutoff2)] <- 0 #Replace NAs with 0s
+head(SRMDataNMDSNormalizedPivotedCorrectedCutoff2) #Confirm there are no NAs
+
+area.t4 <- t(SRMDataNMDSNormalizedPivotedCorrectedCutoff2) #Transpose the file so that rows and columns are switched
+head(area.t4) #Confirm transposition
+area.tra4 <- (area.t4+1) #Add 1 to all values before transforming
+area.tra4 <- data.trans(area.tra4, method = 'log', plot = FALSE) #log(x+1) transformation
+
+
 #### CUTOFF = 0.8, NONNORMALIZED DATA ####
+#Since my past two attempts showed that normalized data is cleaner than nonnormalized data, I will not be writing code for this section.
 
 #### CUTOFF = 0.8, NORMALIZED DATA ####
+
+#Remove transitions with R-squared values below 0.8 cutoff. Based on analyses in 2017-10-10-Correlating-Transitions-in-Technical-Replicates.R, the following rows (individual transitions) should be removed: 1, 2, 3, 4, 5, 6, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 34, 35, 43, 44, 45, 46, 47, 48, 50, 52, 55, 56, 57, 64, 67, 68, 69, 70, 72, 73, 74, 75, 76, 77, 78, 79, 80, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 103, 104, 106, 107, 108, 109, 110, 111
