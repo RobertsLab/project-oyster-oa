dc3fdc060ef3302544abe22c4ae44ae9b67bf5ad
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-10-24 10:24:05 -0700

    cannot create CI around x = y line since there’s no error…

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Confidence-Interval-Transitions/2017-10-24-NMDS-for-Technical-Replication-with-Confidence-Intervals.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Confidence-Interval-Transitions/2017-10-24-NMDS-for-Technical-Replication-with-Confidence-Intervals.R
@@ -1,7 +1,8 @@
 #In this script, I'll regress my technical replicate batches against eachother, then plot an x = y line. If more than 95% of my points fall outside of a 95% CI, I will remove that transition from analysis. Finally, I'll remake my NMDS plot to see if anything improves.
 
 #### SET WORKING DIRECTORY ####
-setwd(dir = "/Users/yaaminivenkataraman/Documents/project-oyster-oa/analyses/DNR_SRM_20170902/") #Set the working directory to the folder with all SRM analysis files.
+setwd(dir = "../..") #Set the working directory to the folder with all SRM analysis files (project-oyster-oa/analyses/DNR_SRM_20170902)
+getwd()
 
 #### IMPORT DATA ####
 
@@ -82,7 +83,25 @@ head(SRMDataTransposedReplicateOne) #Confirm transposition
 SRMDataTransposedReplicateTwo <- t(SRMDataTargetsReplicateTwo) #Transpose Replicate 2 dataframe
 head(SRMDataTransposedReplicateTwo) #Confirm transposition
 
+#### CREATE A NULL CONFIDENCE INTERVAL MODEL ####
+#I FOUND THIS DOESN'T WORK BECAUSE A CONFIDENCE INTERVAL AROUND A PERFECT MODEL DOESN'T EXIST.
+#In order to make a 95% confidence interval around a line, I need a model to work with.
+
+#confidenceIntervalData <- data.frame(x = c(1:7000),y = c(1:7000)) #Create a dataframe with data for a confidence interval around a x = y line
+#confidenceIntervalModel <- lm(confidenceIntervalData$y ~ confidenceIntervalData$x) #Use x values to predict y values in model
+
+#### CALCULATE CONFIDENCE INTERVAL ####
+#I need to know the MSE and SSX to calculate my confidence interval. I can calculate these values outside of my loop function since these values are not going to change between transitions.
+
+#mse <- (summary(confidenceIntervalModel)$sigma)^2 #Calculate MSE using residual standard error from summary statistics
+#ssx <- sum((confidenceIntervalData$x - mean(confidenceIntervalData$x))^2) #Calculate the sum of squares for x values
+#x <- seq(from = min(confidenceIntervalData$x), to = max(confidenceIntervalData$x), by = 1) #Create a sequence of all possible x values
+#y.fitted <- summary(confidenceIntervalModel)$coeff[1] + summary(confidenceIntervalModel)$coeff[2]*x #Formula for prediction line
+#y.upper <- y.fitted + sqrt(2*qf(0.95, 2, 44))*sqrt(mse*(1/12 + (x - mean(confidenceIntervalData$x))^2/ssx)) #Formula for upper confidence bound. Generate critical value from F distribution, where alpha = 0.95, numerator df = 2 (estimating intercept and slope), residual standard error df = 44 (46 samples - 2).
+#y.lower <- y.fitted - sqrt(2*qf(0.95, 2, 44))*sqrt(mse*(1/12 + (x - mean(confidenceIntervalData$x))^2/ssx)) #Lower confidence bound
+
 #### CREATE PLOTS FOR EACH TRANSITION ####
+#I will regress my second batch of technical replicates against my first, plot the regression line, and the x = y line with its confidence interval.
 
 setwd(dir = "2017-10-10-Troubleshooting/2017-10-24-Confidence-Interval-Transitions/") #Change working directory so files are saved in the same directory as the R script
 getwd() #Confirm changes
@@ -100,15 +119,8 @@ for(i in 1:nTransitions) { #For all transitions
   plot(x= SRMDataTransposedReplicateOne[,i], y = SRMDataTransposedReplicateTwo[,i], xlab = "Replicate 1 Area", ylab = "Replicate 2 Area", main = correlationFilenames$filename[i], type = "n") #Create plot, but do not plot points
   text(x = SRMDataTransposedReplicateOne[,i], y = SRMDataTransposedReplicateTwo[,i], labels = rownames(SRMDataTransposedReplicateOne), cex = 0.7) #Plot sample ID instead of points
   abline(transitionModel, col = "red") #Plot regression
-  abline(a = 0, b = 1) #Plot x = y line
   legend("topleft", bty = "n", legend = paste("R2 =", format(summary(transitionModel)$adj.r.squared, digits=4))) #Plot R-squared value
+  abline(a = 0, b  = 1, col = "blue")
+  legend("bottomright", bty = "n", legend = paste("fitted line =", format(summary(transitionModel)$coeff[1], digits = 4), "+", format(summary(transitionModel)$coeff[2], digits = 4), "*x"))
   dev.off() #Turn off plotting mechanism
 }
-
-
-transitionModel <- lm(SRMDataTransposedReplicateTwo[,1] ~ SRMDataTransposedReplicateOne[,1]) #Predict Replicate 2 from Replicate 1
-plot(x= SRMDataTransposedReplicateOne[,1], y = SRMDataTransposedReplicateTwo[,1], xlab = "Replicate 1 Area", ylab = "Replicate 2 Area", main = correlationFilenames$filename[1], type = "n") #Create plot, but do not plot points
-text(x = SRMDataTransposedReplicateOne[,1], y = SRMDataTransposedReplicateTwo[,1], labels = rownames(SRMDataTransposedReplicateOne), cex = 0.7) #Plot sample ID instead of points
-abline(transitionModel, col = "red") #Plot regression
-abline(a = 0, b = 1, col = "blue") #Plot x = y line
-legend("topleft", bty = "n", legend = paste("R2 =", format(summary(transitionModel)$adj.r.squared, digits=4))) #Plot R-squared value
