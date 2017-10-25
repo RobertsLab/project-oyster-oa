01324460de0ec1e554abbe9402bccb0c6e306fe2
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-10-25 11:43:19 -0700

    assessed technical replication after CV filtering

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-NMDS-for-Technical-Replication-after-CV-Filtering.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-NMDS-for-Technical-Replication-after-CV-Filtering.R
@@ -12,61 +12,47 @@ head(SRMModifiedAreas) #Confirm import. There are six columns. The first column 
 SRMModifiedAreas <- SRMModifiedAreas[,-c(1, 6)] #Remove unnecessary columns
 head(SRMModifiedAreas) #Confirm changes. Now I only have Protein.Name, Sample, Replicate1 and Replicate2 (peak areas from Skyline, which are a proxy for protein abundance)
 
+#### REMFORMAT DATA ####
 
+SRMModifiedAreasReplicate1 <- SRMModifiedAreas[, 1:3] #Duplicate dataframe, keeping only Replicate 1 information
+SRMModifiedAreasReplicate1$Sample.Number <- paste(SRMModifiedAreasReplicate1$Sample, "-1") #Create a new column with sample and replicate number
+SRMModifiedAreasReplicate1 <- SRMModifiedAreasReplicate1[, -2] #Remove Sample column
+colnames(SRMModifiedAreasReplicate1) <- c("Protein.Name", "Area", "Sample") #Change column names
+head(SRMModifiedAreasReplicate1) #Confirm changes
 
+SRMModifiedAreasReplicate2 <- SRMModifiedAreas[, c(1, 2, 4)] #Duplicate dataframe, keeping only Replicate 2 information
+SRMModifiedAreasReplicate2$Sample.Number <- paste(SRMModifiedAreasReplicate2$Sample, "-2") #Create a new column with sample and replicate number
+SRMModifiedAreasReplicate2 <- SRMModifiedAreasReplicate2[, -2] #Remove Sample column
+colnames(SRMModifiedAreasReplicate2) <- c("Protein.Name", "Area", "Sample") #Change column names
+head(SRMModifiedAreasReplicate2) #Confirm changes
 
+SRMModifiedAreasLong <- rbind(SRMModifiedAreasReplicate1, SRMModifiedAreasReplicate2) #Paste dataframes together
+head(SRMModifiedAreasLong) #Confirm changes
+tail(SRMModifiedAreasLong) #Confirm changes
+transform(SRMModifiedAreasLong, Area = as.numeric(Area)) #Make sure Area is recognized as a numeric variable
+is.numeric(SRMModifiedAreasLong$Area) #Confirm change
 
+library(reshape2) #Install package to pivot table
+SRMModifiedAreasPivoted <- dcast(SRMModifiedAreasLong, Protein.Name ~ Sample, value.var = "Area") #Cast table! Protein.Name remains as a column with Sample Number as column headers. Area column defined as values.
+head(SRMModifiedAreasPivoted) #Confirm cast
+rownames(SRMModifiedAreasPivoted) <- SRMModifiedAreasPivoted[, 1] #Save Protein.Name column as rownames
+SRMModifiedAreasPivoted <- SRMModifiedAreasPivoted[, -1] #Remove Protein.Name column
+head(SRMModifiedAreasPivoted) #Confirm changes
 
+SRMModifiedAreasPivotedCorrected <- SRMModifiedAreasPivoted #Duplicate dataframe
+SRMModifiedAreasPivotedCorrected[is.na(SRMModifiedAreasPivotedCorrected)] <- 0 #Replace NAs with 0s
+head(SRMModifiedAreasPivotedCorrected) #Confirm there are no NAs
 
-
-
-#### SUBSET DATA FOR NMDS PLOT ####
-
-#For the NMDS, I want only the protein/peptide/transition information and peak area
-
-SRMDataNMDS <- masterSRMDataBiologicalReplicates #Duplicate master list into a new dataframe
-head(SRMDataNMDS) #Confirm copy
-tail(SRMDataNMDS) #Confirm copy
-SRMDataNMDS <- SRMDataNMDS[,-c(2, 5, 7, 10, 11)] #Remove extraneous columns: Replicate.Name, Transition, Peptide.Retention.Time, Site, Eelgrass
-head(SRMDataNMDS) #Confirm column removal
-SRMDataNMDS <- SRMDataNMDS[! SRMDataNMDS$Protein.Name %in% "PRTC peptides", ] #Remove PRTC peptide data
-head(SRMDataNMDS) #Confirm removal
-transform(SRMDataNMDS, Area = as.numeric(Area)) #Make sure Area is recognized as a numeric variable
-is.numeric(SRMDataNMDS$Area) #Confirm change
-transform(SRMDataNMDS, TIC = as.numeric(TIC)) #Make sure TIC is recognized as a numeric variable
-is.numeric(SRMDataNMDS$TIC) #Confirm change
-
-#### MAKE NMDS WITHOUT NORMALIZING ####
-
-#The goal is to have the row names of my new dataframe be Protein/Peptides/Transitions, with the column names as the sample number
-
-SRMDataNonNormalizedNMDS <- SRMDataNMDS #Create a duplicate dataframe
-SRMDataNonNormalizedNMDS <- SRMDataNMDS[,-6] #Remove TIC column
-head(SRMDataNonNormalizedNMDS) #Confirm creation
-
-#My first step is to change my dataframe from long to wide (i.e. cast it)
-library(reshape2) #Instal package to pivot table
-SRMDataNMDSNonNormalizedPivoted <- dcast(SRMDataNonNormalizedNMDS, Protein.Name + Peptide.Sequence + Fragment.Ion ~ Sample.Number) #Cast table! Protein/Peptides/Transitions remain as columns with Sample Number as column headers. Normalized.Area used as value column by default.
-head(SRMDataNMDSNonNormalizedPivoted) #Confirm cast.
-SRMDataNMDSNonNormalizedPivoted$RowNames <- paste(SRMDataNMDSNonNormalizedPivoted$Protein.Name, SRMDataNMDSNonNormalizedPivoted$Peptide.Sequence, SRMDataNMDSNonNormalizedPivoted$Fragment.Ion) #Merge Protein, Peptide and Transition information into one column
-head(SRMDataNMDSNonNormalizedPivoted) #Confirm column merge
-SRMDataNMDSNonNormalizedPivoted <- SRMDataNMDSNonNormalizedPivoted[,-c(1:3)] #Remove unmerged columns
-head(SRMDataNMDSNonNormalizedPivoted) #Confirm column removal
-#write.csv(SRMDataNMDSNonNormalizedPivoted, file = "2017-09-07-SRM-Data-NMDS-Pivoted.csv") #Wrote out as .csv to make future analyses easier.
-
-#Now I can make an NMDS plot
+#### LOAD FUNCTIONS NEEDED FOR NMDS ####
 
 #Load the source file for the biostats package
 source("biostats.R") #Either load the source R script or copy paste
 install.packages("vegan") #Install vegan package
 library(vegan)
 
-SRMDataNMDSNonNormalizedPivotedCorrected <- SRMDataNMDSNonNormalizedPivoted #Duplicate dataframe
-SRMDataNMDSNonNormalizedPivotedCorrected[is.na(SRMDataNMDSNonNormalizedPivotedCorrected)] <- 0 #Replace NAs with 0s
-head(SRMDataNMDSNonNormalizedPivotedCorrected) #Confirm there are no NAs
+#### MAKE NMDS PLOT ####
 
-area.protID <- SRMDataNMDSNonNormalizedPivotedCorrected[-93] #Save all area data as a new dataframe
-rownames(area.protID) <- SRMDataNMDSNonNormalizedPivotedCorrected[,93] #Make sure last column of protein names is recognized as row names instead of values
+area.protID <- SRMModifiedAreasPivotedCorrected #Save all area data as a new dataframe
 head(area.protID) #Confirm changes
 
 area.t <- t(area.protID) #Transpose the file so that rows and columns are switched
@@ -74,36 +60,22 @@ head(area.t) #Confirm transposition
 area.tra <- (area.t+1) #Add 1 to all values before transforming
 area.tra <- data.trans(area.tra, method = 'log', plot = FALSE) #log(x+1) transformation
 
-proc.nmds.nonnorm.euclidean <- metaMDS(area.t, distance = 'euclidean', k = 2, trymax = 10000, autotransform = FALSE) #Make MDS dissimilarity matrix using euclidean distance. Julian confirmed that I should use euclidean distances, and not bray-curtis
-#stressplot(proc.nmds.nonnorm.euclidean) #Make Shepard plot
-#ordiplot(proc.nmds.nonnorm.euclidean) #Plot basic NMDS
-#vec.proc.nmds.nonnorm.euclidean <- envfit(proc.nmds.nonnorm.euclidean$points, area.t, perm = 1000) #Calculate loadings
-ordiplot(proc.nmds.nonnorm.euclidean, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
-#plot(vec.proc.nmds.euclidean, p.max=.01, col='blue') #Plot eigenvectors
+proc.nmds.norm.euclidean <- metaMDS(area.t, distance = 'euclidean', k = 2, trymax = 10000, autotransform = FALSE) #Make MDS dissimilarity matrix using euclidean distance. Julian confirmed that I should use euclidean distances, and not bray-curtis
+stressplot(proc.nmds.norm.euclidean) #Make Shepard plot
+#ordiplot(proc.nmds.norm.euclidean) #Plot basic NMDS
+#vec.proc.nmds.norm.euclidean <- envfit(proc.nmds.nonnorm.euclidean$points, area.t, perm = 1000) #Calculate loadings
+#jpeg(filename = "2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-NMDS-TechnicalReplication-Normalized-after-CV-Filtering.jpeg", width = 1000, height = 1000) #Save plot
+ordiplot(proc.nmds.norm.euclidean, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
+#plot(vec.proc.nmds.norm.euclidean, p.max=.01, col='blue') #Plot eigenvectors
+#dev.off() #Turn off plotting mechanism
 
-#proc.nmds.nonnorm.euclidean.log <- metaMDS(area.tra, distance = 'euclidean', k = 2, trymax = 10000, autotransform = FALSE) #Make MDS dissimilarity matrix using euclidean distance
-#stressplot(proc.nmds.nonnorm.euclidean.log) #Make Shepard plot
-#ordiplot(proc.nmds.nonnorm.euclidean.log) #Plot basic NMDS
-#ordiplot(proc.nmds.nonnorm.euclidean.log, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
-
-#proc.nmds.nonnorm.euclidean.autotransform <- metaMDS(area.t, distance = 'euclidean', k = 2, trymax = 10000, autotransform = TRUE) #Make MDS dissimilarity matrix using euclidean distance and autotransformation
-#stressplot(proc.nmds.nonnorm.euclidean.autotransform) #Make Shepard plot
-#ordiplot(proc.nmds.nonnorm.euclidean.autotransform) #Plot basic NMDS
-#ordiplot(proc.nmds.nonnorm.euclidean.autotransform, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
-
-#jpeg(filename = "2017-09-11-NMDS-TechnicalReplication-NonNormalized.jpeg", width = 1000, height = 1000)
-#ordiplot(proc.nmds.nonnorm.euclidean, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
+proc.nmds.norm.euclidean.log <- metaMDS(area.tra, distance = 'euclidean', k = 2, trymax = 10000, autotransform = FALSE) #Make MDS dissimilarity matrix using euclidean distance
+stressplot(proc.nmds.norm.euclidean.log) #Make Shepard plot
+#jpeg(filename = "2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-NMDS-TechnicalReplication-Normalized-LogTransformed-after-CV-Filtering.jpeg", width = 1000, height = 1000) #Save plot
+ordiplot(proc.nmds.norm.euclidean.log, choices = c(1,2), type = "text", display = "sites") #Plot refined NMDS displaying only samples with their names
 #dev.off()
 
-#Only the euclidean non-transformed version gives me an NMDS plot, but there looks like there's variation that should be accounted for by normalizing.
-
-#### NORMALIZE BY TIC VALUES ####
-
-SRMNormalizedDataNMDS <- SRMDataNMDS #Duplicate dataframe
-SRMNormalizedDataNMDS$Normalized.Area <- SRMNormalizedDataNMDS$Area/SRMDataNMDS$TIC #Divide areas by corresponding TIC values
-head(SRMNormalizedDataNMDS) #Confirm division
-SRMNormalizedDataNMDS <- SRMNormalizedDataNMDS[,-c(5,6)] #Remove nonnormalized area and TIC columns
-head(SRMNormalizedDataNMDS) #Confirm column removal
+#My technical replication looks SO MUCH BETTER! It's still not fantastic, but my samples are clearly lining up. I saved both the nontransformed and log transformed plots. Next, I'm going to use the nontransformed NMDS ordinations to calculate distances between my technical replicates.
 
 #### REFORMAT DATAFRAME FOR NMDS ####
 
