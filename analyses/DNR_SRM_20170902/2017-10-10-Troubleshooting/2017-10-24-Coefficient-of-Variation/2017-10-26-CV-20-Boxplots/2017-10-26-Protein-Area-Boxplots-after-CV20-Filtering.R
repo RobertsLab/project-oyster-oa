6940b940a9284ece75482d7ee84f533455588d67
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-10-26 14:49:03 -0700

    started working on script to make box plots

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-CV-20-Boxplots/2017-10-26-Protein-Area-Boxplots-after-CV20-Filtering.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-CV-20-Boxplots/2017-10-26-Protein-Area-Boxplots-after-CV20-Filtering.R
@@ -1,8 +1,18 @@
-#Before going through this script, I went through "2017-09-10-NMDS-ANOSIM-for-Cluster-Analysis." In this script, I'll depict normalized protein area across samples as bar charts
+#In this script, I'll depict normalized protein area across samples as bar charts after filtering transitions with a coefficient of variation > 20 and technical replicates with ordination distances > 0.20.
 
-#### DATA MANIPULATION ####
+#### SET WORKING DIRECTORY ####
+setwd("../../..") #Set working directory to the master SRM folder
+getwd()
 
-SRMDataNMDSAveragedCorrected #From 2017-09-06-NMDS-for-Technical-Replication. Average normalized area data.
+#### IMPORT DATA ####
+
+SRMDataNMDSAveragedCorrected <- read.csv("2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-Averaged-Pivoted-Corrected-SRM-Data-after-CV20-and-Distance-Filtering.csv", header = TRUE) #Import modified dataset. This dataset has rownames as the first column, column names as sample IDs. Area data is averaged and corrected (no NAs)
+rownames(SRMDataNMDSAveragedCorrected) <- SRMDataNMDSAveragedCorrected[,1]
+SRMDataNMDSAveragedCorrected <- SRMDataNMDSAveragedCorrected[,-1] #Remove first column of rownames
+head(SRMDataNMDSAveragedCorrected) #Confirm import.
+
+#### REFORMAT DATA ####
+
 boxplotData <- data.frame(t(SRMDataNMDSAveragedCorrected)) #Transpose the data
 boxplotData$Sample.Number <- rownames(boxplotData) #Save rownames as a new column
 head(boxplotData) #Confirm changes
@@ -25,8 +35,13 @@ head(boxplotData) #Confirm changes
 #### MAKE BOXPLOTS JUST BASED ON SITES ####
 
 nTransitions <- (length(boxplotData)) #The number of columns in the dataframe. The first 2 columns are Site and Eelgrass.Condition
-boxplotFilenames <- data.frame(filenames = colnames(boxplotData),
-                               modifier = rep("Eelgrass", 113)) #Make filename sheet
+#boxplotFilenames <- data.frame(filenames = colnames(boxplotData),
+                               modifier = rep("Eelgrass", 113)) #Make filename sheet NEED TO FIX FILENAMES
+
+
+
+
+
 for(i in 3:nTransitions) { #For all of my columns with transition IDs
   fileName <- boxplotFilenames$filenames[i] #Set the file name choices as the first column
   jpeg(filename = fileName, width = 1000, height = 1000) #Save using set file name
