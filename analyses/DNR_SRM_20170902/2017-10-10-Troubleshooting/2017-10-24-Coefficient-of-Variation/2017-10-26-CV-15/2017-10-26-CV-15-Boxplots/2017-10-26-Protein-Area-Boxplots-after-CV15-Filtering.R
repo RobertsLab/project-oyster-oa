9aecdee02beb17a63c24ca7fe9592aa14f2c15a8
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-10-26 18:11:29 -0700

    wrote out file needed for CV15 filtering

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-CV-15/2017-10-26-CV-15-Boxplots/2017-10-26-Protein-Area-Boxplots-after-CV15-Filtering.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-CV-15/2017-10-26-CV-15-Boxplots/2017-10-26-Protein-Area-Boxplots-after-CV15-Filtering.R
@@ -1,34 +1,55 @@
-#Before going through this script, I went through "2017-09-10-NMDS-ANOSIM-for-Cluster-Analysis." In this script, I'll depict normalized protein area across samples as bar charts
+#In this script, I'll depict normalized protein area across samples as bar charts after filtering transitions with a coefficient of variation > 15 and technical replicates with ordination distances > 0.20.
 
-#### DATA MANIPULATION ####
+#### SET WORKING DIRECTORY ####
+setwd("../../..") #Set working directory to the master SRM folder
+getwd()
 
-SRMDataNMDSAveragedCorrected #From 2017-09-06-NMDS-for-Technical-Replication. Average normalized area data.
-boxplotData <- data.frame(t(SRMDataNMDSAveragedCorrected)) #Transpose the data
-boxplotData$Sample.Number <- rownames(boxplotData) #Save rownames as a new column
-head(boxplotData) #Confirm changes
+#### IMPORT DATA ####
 
-biologicalReplicates <- read.csv("2017-09-06-Biological-Replicate-Information.csv", na.strings = "N/A") #Import site and eelgrass condition information (i.e. biological replicate information)
+SRMDataNMDSAveragedCorrected <- read.csv("2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-Averaged-Pivoted-Corrected-SRM-Data-after-CV20-and-Distance-Filtering.csv", header = TRUE) #Import modified dataset. This dataset has rownames as the first column, column names as sample IDs. Area data is averaged and corrected (no NAs)
+rownames(SRMDataNMDSAveragedCorrected) <- SRMDataNMDSAveragedCorrected[,1]
+SRMDataNMDSAveragedCorrected <- SRMDataNMDSAveragedCorrected[,-1] #Remove first column of rownames
+head(SRMDataNMDSAveragedCorrected) #Confirm import.
+
+#### REFORMAT DATA ####
+
+SRMDataNMDSAveragedCorrectedTransposed <- data.frame(t(SRMDataNMDSAveragedCorrected)) #Transpose the data
+SRMDataNMDSAveragedCorrectedTransposed$Sample.Number <- rownames(SRMDataNMDSAveragedCorrectedTransposed) #Save rownames as a new column
+head(SRMDataNMDSAveragedCorrectedTransposed) #Confirm changes
+
+biologicalReplicates <- read.csv("2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-Biological-Replicate-Information-SampleID-Only.csv", na.strings = "N/A") #Import site and eelgrass condition information (i.e. biological replicate information)
 head(biologicalReplicates) #Confirm import
+colnames(biologicalReplicates) <- c("Sample.Number", "Site", "Eelgrass.Condition")
 rownames(biologicalReplicates) <- biologicalReplicates$Sample.Number #Set sample number as row names
 head(biologicalReplicates) #Confirm changes
-biologicalReplicates <- biologicalReplicates[-c(50,100),] #Remove blanks
 biologicalReplicates$Site <- factor(biologicalReplicates$Site) #Remove 0 as a factor
 biologicalReplicates$Eelgrass.Condition <- factor(biologicalReplicates$Eelgrass.Condition) #Remove 0 as a factor
 str(biologicalReplicates) #Confirm factor reset
 
-boxplotData <- merge(x = biologicalReplicates, y = boxplotData, by = "Sample.Number") #Merge together
+boxplotData <- merge(x = biologicalReplicates, y = SRMDataNMDSAveragedCorrectedTransposed, by = "Sample.Number") #Merge together
 head(boxplotData) #Confirm merge
 rownames(boxplotData) <- boxplotData$Sample.Number #Set sample number as row names
 boxplotData <- boxplotData[-1] #Remove Sample.Number column
 head(boxplotData) #Confirm changes
 
+#### ASSIGN FILENAMES ####
+
+boxplotFilenames <- data.frame(protein = colnames(boxplotData),
+                               modifier = rep("Eelgrass", length(boxplotData))) #Make filename sheet
+boxplotFilenames$siteFilenames <- paste(boxplotFilenames$protein, ".jpeg") #Make a new column for the site only filenames
+boxplotFilenames$eelgrassFilenames <-paste(boxplotFilenames$protein, boxplotFilenames$modifier, ".jpeg") #Mae a new column for the site and eelgrass filenames
+head(boxplotFilenames) #Confirm changes
+
+#### CHANGE WORKING DIRECTORY ####
+
+setwd("2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-26-CV-20-Boxplots/")
+getwd()
+
 #### MAKE BOXPLOTS JUST BASED ON SITES ####
 
 nTransitions <- (length(boxplotData)) #The number of columns in the dataframe. The first 2 columns are Site and Eelgrass.Condition
-boxplotFilenames <- data.frame(filenames = colnames(boxplotData),
-                               modifier = rep("Eelgrass", 113)) #Make filename sheet
 for(i in 3:nTransitions) { #For all of my columns with transition IDs
-  fileName <- boxplotFilenames$filenames[i] #Set the file name choices as the first column
+  fileName <- boxplotFilenames$siteFilenames[i] #Set the file name choices as the first column
   jpeg(filename = fileName, width = 1000, height = 1000) #Save using set file name
   boxplot(boxplotData[,i] ~ boxplotData$Site, xlab = "Sites", ylab = "Abundance") #Create the boxplot
   dev.off() #Close file
@@ -36,9 +57,8 @@ for(i in 3:nTransitions) { #For all of my columns with transition IDs
 
 #### MAKE BOXPLOTS BASED ON SITES AND EELGRASS CONDITIONS ####
 
-boxplotFilenames$Eelgrass <- paste(boxplotFilenames$filenames, boxplotFilenames$modifier) #Create unique names for eelgrass plots
 for(i in 3:nTransitions) { #For all of my columns with transition IDs
-  fileName <- boxplotFilenames$Eelgrass[i] #Set the file name choices as the third column
+  fileName <- boxplotFilenames$eelgrassFilenames[i] #Set the file name choices as the third column
   jpeg(filename = fileName, width = 1000, height = 1000) #Save using set file name
   boxplot(boxplotData[,i] ~ boxplotData$Site + boxplotData$Eelgrass.Condition, xlab = "Sites", ylab = "Abundance", names = c("CI.Bare", "FB.Bare", "PG.Bare", "SK.Bare", "WB.Bare", "CI.Eelgrass", "FB.Eelgrass", "PG.Eelgrass", "SK.Eelgrass", "WB.Eelgrass")) #Create the boxplot
   dev.off() #Close file
