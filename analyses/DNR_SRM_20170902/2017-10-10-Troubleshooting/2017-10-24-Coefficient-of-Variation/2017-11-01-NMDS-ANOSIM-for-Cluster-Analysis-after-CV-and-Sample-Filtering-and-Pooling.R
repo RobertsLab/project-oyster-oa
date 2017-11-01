611725b550f5928ee6936ec73b027968c2806a9c
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-11-01 15:27:16 -0700

    reran ANOSIM with pooled samples

--- analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-11-01-NMDS-ANOSIM-for-Cluster-Analysis-after-CV-and-Sample-Filtering-and-Pooling.R
+++ analyses/DNR_SRM_20170902/2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-11-01-NMDS-ANOSIM-for-Cluster-Analysis-after-CV-and-Sample-Filtering-and-Pooling.R
@@ -1,4 +1,4 @@
-#Steven went through my normalized data set of technical replicates and calculated a coefficient of variation for each transition. He removed any transitions that had CVs greater than 20. I then went through and removed any samples missing more than half of its transition data. Using this dataset, I'll average my technical replicates and proceed with an NMDS and ANOSIM.
+#Steven went through my normalized data set of technical replicates and calculated a coefficient of variation for each transition. He removed any transitions that had CVs greater than 20. I then went through and removed any samples missing more than half of its transition data. Using this dataset, I'm going to pool my bare and eelgrass samples and remake my NMDS.
 
 #### SET WORKING DIRECTORY ####
 
@@ -51,56 +51,48 @@ temporaryData <- data.frame(sampleIDs = sampleIDs,
                             y = rep(x = 0, times = length(sampleIDs))) #Create a temporary dataframe with sample IDs used in NMDS
 head(temporaryData) #Confirm dataframe creation
 
-NMDSColorShapeCustomization <- merge(x = temporaryData, y = biologicalReplicates, by = "sampleIDs") #Merge biological information with samples used
-head(NMDSColorShapeCustomization) #Confirm merge
-tail(NMDSColorShapeCustomization) #Confirm merge
-NMDSColorShapeCustomization <- NMDSColorShapeCustomization[, -2] #Remove empty column
-tail(NMDSColorShapeCustomization) #Confirm removal
-NMDSColorShapeCustomization$sampleIDs #Confirm all sample IDs are there
+NMDSColorCustomization <- merge(x = temporaryData, y = biologicalReplicates, by = "sampleIDs") #Merge biological information with samples used
+head(NMDSColorCustomization) #Confirm merge
+tail(NMDSColorCustomization) #Confirm merge
+NMDSColorCustomization <- NMDSColorCustomization[, -2] #Remove empty column
+tail(NMDSColorCustomization) #Confirm removal
+NMDSColorCustomization$sampleIDs #Confirm all sample IDs are there
+colnames(NMDSColorCustomization) <- c("sample", "site", "eelgrassCondition") #Rename first sample column
+head(NMDSColorCustomization) #Confirm changes
 
 #Create a color and shape palette
-attach(NMDSColorShapeCustomization)
-NMDSColorShapeCustomization <- NMDSColorShapeCustomization[order(site),] #Reorder so sites are sorted alphabetically
-head(NMDSColorShapeCustomization) #Confirm sorting
-detach(NMDSColorShapeCustomization)
-NMDS.Colors <- c(rep(x = "red", times = sum(NMDSColorShapeCustomization$site == "CI")),
-                 rep(x = "blue", times = sum(NMDSColorShapeCustomization$site == "FB")),
-                 rep(x = "black", times = sum(NMDSColorShapeCustomization$site == "PG")),
-                 rep(x = "green", times = sum(NMDSColorShapeCustomization$site == "SK")),
-                 rep(x = "magenta", times = sum(NMDSColorShapeCustomization$site == "WB"))) #Create a color vector
-NMDSColorShapeCustomization[,4] <- NMDS.Colors #Add the color vector to the dataframe
-head(NMDSColorShapeCustomization) #Confirm addition
-attach(NMDSColorShapeCustomization)
-NMDSColorShapeCustomization <- NMDSColorShapeCustomization[order(eelgrassCondition),] #Reorder so eelgrass condition is sorted alphabetically
-head(NMDSColorShapeCustomization) #Confirm sorting
-detach(NMDSColorShapeCustomization)
-NMDS.Shapes <- c(rep(x = 16, times = sum(NMDSColorShapeCustomization$eelgrassCondition == "Bare")),
-                 rep(x = 17, times = sum(NMDSColorShapeCustomization$eelgrassCondition == "Eelgrass"))) #Make a shape vector
-NMDSColorShapeCustomization[,5] <- NMDS.Shapes #Add the shape vector to the dataframe
-head(NMDSColorShapeCustomization) #Confirm addition
-#attach(NMDSColorShapeCustomization)
-#NMDSColorShapeCustomization <- NMDSColorShapeCustomization[order(sampleIDs),] #Resort by sample number
-#head(NMDSColorShapeCustomization) #Confirm sorting
-#detach(NMDSColorShapeCustomization)
-colnames(NMDSColorShapeCustomization) <- c("Sample.Number", "Site", "Eelgrass.Condition", "Color", "Shape") #Change column names
-head(NMDSColorShapeCustomization) #Confirm change
+attach(NMDSColorCustomization)
+NMDSColorCustomization <- NMDSColorCustomization[order(site),] #Reorder so sites are sorted alphabetically
+head(NMDSColorCustomization) #Confirm sorting
+detach(NMDSColorCustomization)
+NMDS.Colors <- c(rep(x = "red", times = sum(NMDSColorCustomization$site == "CI")),
+                 rep(x = "blue", times = sum(NMDSColorCustomization$site == "FB")),
+                 rep(x = "magenta", times = sum(NMDSColorCustomization$site == "PG")),
+                 rep(x = "green", times = sum(NMDSColorCustomization$site == "SK")),
+                 rep(x = "black", times = sum(NMDSColorCustomization$site == "WB"))) #Create a color vector
+NMDSColorCustomization[,3] <- NMDS.Colors #Add the color vector to the dataframe
+head(NMDSColorCustomization) #Confirm addition
+attach(NMDSColorCustomization)
+NMDSColorCustomization <- NMDSColorCustomization[order(sample),] #Reorder so samples are sorted alphabetically
+head(NMDSColorCustomization) #Confirm sorting
+detach(NMDSColorCustomization)
+colnames(NMDSColorCustomization) <- c("Sample.Number", "Site", "Color") #Change column names
+head(NMDSColorCustomization) #Confirm change
 
 #### NMDS REFINEMENT ####
 
-#jpeg(filename = "2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-11-01-NMDS-Norm-Analysis-Averaged.jpeg", width = 1000, height = 1000)
+#jpeg(filename = "2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-11-01-NMDS-Norm-Analysis-Averaged-Pooled.jpeg", width = 1000, height = 1000)
 fig.nmds <- ordiplot(proc.nmds.norm.averaged.euclidean, choices = c(1,2), type = "none", display = "sites", xlab= "Axis 1", ylab= "Axis 2", cex = 0.5) #Save NMDS as a new object
 
 #Legend for NMDS plot:
-#Bare = circle
-#Eelgrass = Triangle
 #Case Inlet = Red
 #Fidalgo Bay = Blue
 #Willapa Bay = Black
 #Skokomish River Delta = Green
 #Port Gamble Bay = Magenta
 
-points(fig.nmds, "sites", col = NMDSColorShapeCustomization$Color, pch = NMDSColorShapeCustomization$Shape)
-legend("topright", cex = .5, pch = c(rep(x = 16, times = 6), 17), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble", "Bare", "Eelgrass"), col=c('red', 'blue', 'black', 'green', 'magenta', 'black', 'black'))
+points(fig.nmds, "sites", col = NMDSColorCustomization$Color, pch = 16)
+legend("topright", cex = .5, pch = c(rep(x = 16, times = 5)), legend=c('Case Inlet', "Fidalgo Bay", "Willapa Bay", "Skokomish", "Port Gamble"), col=c('red', 'blue', 'black', 'green', 'magenta'))
 title("Protein Expression Similarities between Sites and Habitats")
 #dev.off()
 
@@ -112,23 +104,10 @@ row.names(ANOSIMReplicates) <- ANOSIMReplicates[,1] #Assign sample numbers as ro
 ANOSIMReplicates <- ANOSIMReplicates[,-1] #Remove Sample.Number column
 head(ANOSIMReplicates) #Confirm changes
 
-ANOSIMReplicates$eelgrassCondition <- factor(ANOSIMReplicates$eelgrassCondition) #Make sure residual factors are no longer present
 ANOSIMReplicates$site <- factor(ANOSIMReplicates$site) #Make sure residual factors are no longer present
 str(ANOSIMReplicates) #Confirm new factors
-
-ANOSIMReplicates$siteEelgrass <- paste(ANOSIMReplicates$site, ANOSIMReplicates$eelgrassCondition) #Create third column for two-way ANOSIM
-head(ANOSIMReplicates) #Confirm changes
 
 siteNormANOSIM <- anosim(dat = dissimArea.t, grouping = ANOSIMReplicates[,1]) #One-way ANOSIM by Site presence
 summary(siteNormANOSIM)
 plot(siteNormANOSIM)
 #simper(proc.nmds.norm.averaged.euclidean, ANOSIMReplicates$site) #Error in rowSums(comm, na.rm = TRUE): 'x' must be an array of at least two dimensions
-
-eelgrassNormANOSIM <- anosim(dat = dissimArea.t, grouping = ANOSIMReplicates[,2]) #One-way ANOSIM by Eelgrass presence
-summary(eelgrassNormANOSIM)
-plot(eelgrassNormANOSIM)
-#simper(proc.nmds.norm.averaged.euclidean, ANOSIMReplicates$eelgrassCondition) #Error in rowSums(comm, na.rm = TRUE): 'x' must be an array of at least two dimensions
-
-siteEelgrassANOSIM <- anosim(dat = dissimArea.t, grouping = ANOSIMReplicates[,3]) #Two-way ANOSIM by eelgrass and site
-summary(siteEelgrassANOSIM)
-plot(siteEelgrassANOSIM)
