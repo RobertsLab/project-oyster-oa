df89735af50f15365a414c1087e5ee01aa042f4d
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-11-27 17:26:52 -0800

    visualized biomarker data

--- analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-15-Biomarker-Data-Analyses.R
+++ analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-15-Biomarker-Data-Analyses.R
@@ -1,106 +1,47 @@
-#In this script, I'll depict normalized protein area across samples as bar charts after integrating my transitions. These boxplots will be at the peptide level and only differentiated by sites. This is because my NMDS analyses did not show any significant differences in clustering based on habitat type.
+#In this script, I'll visualize biomarker data from Alex that corresponds to my samples.
 
 #### SET WORKING DIRECTORY ####
-setwd("../../..") #Set working directory to the master SRM folder
-getwd()
+#setwd("../../..") #Set working directory to the master SRM folder
+#getwd()
 
 #### IMPORT DATA ####
 
-SRMDataNMDSAveragedCorrected <- read.csv("2017-10-10-Troubleshooting/2017-11-05-Integrated-Dataset/2017-11-05-Averaged-Areas-Pivoted-Corrected.csv", header = TRUE) #Import modified dataset. This dataset has rownames as the first column, column names as sample IDs. Area data is averaged and corrected (no NAs)
-rownames(SRMDataNMDSAveragedCorrected) <- SRMDataNMDSAveragedCorrected[,1]
-SRMDataNMDSAveragedCorrected <- SRMDataNMDSAveragedCorrected[,-1] #Remove first column of rownames
-head(SRMDataNMDSAveragedCorrected) #Confirm import.
+biomarkerData <- read.csv("../../data/DNR/2017-11-21-Alex-Data-Yaamini-Samples-Only.csv") #Import dataset
+biomarkerData <- biomarkerData[,-c(1:3, 5, 8:11, 13:14)] #Remove empty columns and columns I don't need (Round, Spp, Bag, Rep.x, FAvial, topValve, bothValves)
+head(biomarkerData) #Confirm changes
 
-#### REFORMAT DATA ####
-
-SRMDataNMDSAveragedCorrectedTransposed <- data.frame(t(SRMDataNMDSAveragedCorrected)) #Transpose the data
-SRMDataNMDSAveragedCorrectedTransposed$Sample.Number <- rownames(SRMDataNMDSAveragedCorrectedTransposed) #Save rownames as a new column
-head(SRMDataNMDSAveragedCorrectedTransposed) #Confirm changes
-
-biologicalReplicates <- read.csv("2017-10-10-Troubleshooting/2017-10-24-Coefficient-of-Variation/2017-10-25-Biological-Replicate-Information-SampleID-Only.csv", na.strings = "N/A") #Import site and eelgrass condition information (i.e. biological replicate information)
-head(biologicalReplicates) #Confirm import
-colnames(biologicalReplicates) <- c("Sample.Number", "Site", "Eelgrass.Condition")
-rownames(biologicalReplicates) <- biologicalReplicates$Sample.Number #Set sample number as row names
-head(biologicalReplicates) #Confirm changes
-biologicalReplicates$Site <- factor(biologicalReplicates$Site) #Remove 0 as a factor
-biologicalReplicates$Eelgrass.Condition <- factor(biologicalReplicates$Eelgrass.Condition) #Remove 0 as a factor
-str(biologicalReplicates) #Confirm factor reset
-
-boxplotData <- merge(x = biologicalReplicates, y = SRMDataNMDSAveragedCorrectedTransposed, by = "Sample.Number") #Merge together
-head(boxplotData) #Confirm merge
-rownames(boxplotData) <- boxplotData$Sample.Number #Set sample number as row names
-boxplotData <- boxplotData[-1] #Remove Sample.Number column
-head(boxplotData) #Confirm changes
+#FinalHeight = length of shell (mm) at longest point from umbo to edge of shell.
+#TissueMass = dry weight (g) of the soft tissue of the oyster after gill was removed
+#shellThick = thickness of the shell (mm) at the point we crushed in the shell stength test. measured with modified calipers.
+#peakLoad = maximum force sustained by shell (N) measured with an MTS brand force gauge. 
+#Strength = maximum force divided by thickness of shell (N/um), this is the main datapoint of interest for the shell strength analysis, and what I observed to be higher in eelgrass for Olympia oysters, and lower in eelgrass for Pacific oysters
+#delC = ratio of C13/C12, the standard stable isotope unit for carbon. Often used to determine diet sources, range is typically -12 (enriched) per mil to -24 (depleted) per mil for marine-estuarine species. fresh healthy phytoplankton growing around here have a signature of ~-18 to -22, whereas resuspended detritus and terrestrial detritus is more depleted ~-22 to -26. Generally interpret more enriched signatures as reflecting a better quality diet in estuarine systems.
+#C.perc = related to tissue chemistry, % carbon by mass
+#N.perc = % Nitrogen by mass, related to protein content. relatively higher %N is usually a good thing.
+#CN.ratio = ratio of %carbon/%Nitrogen. often used as an indicator of health where a lower ratio represents high protein content per unit mass.
 
 #### ASSIGN FILENAMES ####
 
-boxplotFilenames <- data.frame(protein = colnames(boxplotData),
-                               modifier = rep(".jpeg", length(boxplotData))) #Make filename sheet
-boxplotFilenames$siteFilenames <- paste(boxplotFilenames$protein, boxplotFilenames$modifier) #Make a new column for the site only filenames
+boxplotFilenames <- data.frame(biomarker = c("PRVial", "Site", "Habitat", "Final Shell Height", "Tissue Mass", "Final Shell Thickness", "Peak Load", "Strength", "delC", "Percent C", "delN", "Percent N", "C:N Ratio"),
+                               modifier = rep(".jpeg", length(biomarkerData))) #Make filename sheet
+boxplotFilenames$siteFilenames <- paste(boxplotFilenames$biomarker, boxplotFilenames$modifier) #Make a new column for the site only filenames
 head(boxplotFilenames) #Confirm changes
 
 #### CHANGE WORKING DIRECTORY ####
 
-setwd("2017-10-10-Troubleshooting/2017-11-05-Integrated-Dataset/2017-11-06-Boxplots/")
+setwd("2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-27-Biomarker-Boxplots")
 getwd()
 
-#### MAKE BOXPLOTS JUST BASED ON SITES ####
+#### VISUALIZE BIOMARKER DATA ####
 
-nPeptides <- (length(boxplotData)) #The number of columns in the dataframe. The first 2 columns are Site and Eelgrass.Condition
-for(i in 3:nPeptides) { #For all of my columns with peptide IDs
+nBiomarkers <- (length(biomarkerData)) #The number of columns in the dataframe. The first 3 columns are descriptors.
+for(i in 4:nBiomarkers) { #For all columns with biomarker data
   fileName <- boxplotFilenames$siteFilenames[i] #Set the file name choices as the first column
   jpeg(filename = fileName, width = 1000, height = 750) #Save using set file name
-  boxplot(boxplotData[,i] ~ boxplotData$Site, xlab = "Sites", ylab = "", cex.lab = 2, cex.axis = 1.5) #Create the boxplot
-  title(ylab = "Abundance", line = 2.3, cex.lab = 2) #Add the y-axis label
-  stripchart(boxplotData[,i] ~ boxplotData$Site, vertical = TRUE, method = "jitter", add = TRUE, pch = 20, col = 'blue') #Add each data point
-  siteANOVA <- aov(boxplotData[,i] ~ boxplotData$Site) #Perform an ANOVA to test for significant differences between sites
-  legend("topleft", bty = "n", legend = paste("ANOVA p-value =", format(summary(siteANOVA)[[1]][["Pr(>F)"]][[1]], digits=4))) #Plot p-value from ANOVA
-  title(boxplotFilenames$protein[i])
+  boxplot(biomarkerData[,i] ~ biomarkerData$Site.x, xlab = "Sites", ylab = "", cex.lab = 2, cex.axis = 1.5) #Create the boxplot
+  stripchart(biomarkerData[,i] ~ biomarkerData$Site.x, vertical = TRUE, method = "jitter", add = TRUE, pch = 20, col = 'blue') #Add each data point
+  siteANOVA <- aov(biomarkerData[,i] ~ biomarkerData$Site.x) #Perform an ANOVA to test for significant differences between sites
+  legend("topleft", bty = "n", legend = paste("F =", format(summary(siteANOVA)[[1]][["F value"]][[1]], digits = 4), "p =", format(summary(siteANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add F and p-value from ANOVA
+  title(boxplotFilenames$biomarker[i], cex.main = 3)
   dev.off() #Close file
 }
-
-#### PERFORM TUKEY HSD POST-HOC TEST ####
-#This test can be used to understand where significant ANOVA results come from
-
-siteANOVATukeyResults <- data.frame("Protein.Peptide" = colnames(boxplotData),
-                                    "ANOVA.pvalue" = rep(x = 0, times = length(boxplotData)),
-                                    "FB-CI" = rep(x = 0, times = length(boxplotData)),
-                                    "PG-CI" = rep(x = 0, times = length(boxplotData)),
-                                    "SK-CI" = rep(x = 0, times = length(boxplotData)),
-                                    "WB-CI" = rep(x = 0, times = length(boxplotData)),
-                                    "PG-FB" = rep(x = 0, times = length(boxplotData)),
-                                    "SK-FB" = rep(x = 0, times = length(boxplotData)),
-                                    "WB-FB" = rep(x = 0, times = length(boxplotData)),
-                                    "SK-PG" = rep(x = 0, times = length(boxplotData)),
-                                    "WB-PG" = rep(x = 0, times = length(boxplotData)),
-                                    "WB-SK" = rep(x = 0, times = length(boxplotData))) #Create a dataframe to hold all results
-siteANOVATukeyResults <- siteANOVATukeyResults[-c(1:2),] #Remove the first two rows, since they are not peptides
-head(siteANOVATukeyResults) #Confirm changes
-
-#Perform Tukey HSD
-for(i in 3:nPeptides) { #For all of my columns with peptide IDs
-  siteANOVA <- aov(boxplotData[,i] ~ boxplotData$Site) #Perform an ANOVA to test for significant differences between sites
-  siteANOVATukeyResults[(i-2), 2] <- summary(siteANOVA)[[1]][["Pr(>F)"]][[1]] #Paste ANOVA p-value in table
-  siteTukeyHSD <- TukeyHSD(siteANOVA) #Perform Tukey Honest Significant Difference post-hoc test to determine where ANOVA significance is coming from
-  siteANOVATukeyResults[(i-2),3:12] <- siteTukeyHSD$`boxplotData$Site`[,4] #Paste Tukey results into table
-} #Add all ANOVA and Tukey HSD p-values to the table
-head(siteANOVATukeyResults) #Confirm that tests were completed
-#write.csv(siteANOVATukeyResults, "2017-11-06-OneWayANOVA-TukeyHSD-by-Site-pValues.csv") #Wrote out table for future analyses
-
-#### POWER ANALYSIS ####
-
-#Install dependencies
-install.packages("pwr") #Install the power calculation package
-library(pwr) #Load package
-
-#Determine what kind of power I have for a small, medium or large effect sizes. I have k = 5 groups, roughly n = 7 observations per group, and a significance level of 0.05. A small effect size is denoted by f = 0.1, medium is f = 0.25, and large is f = 0.4. These values are suggested by the creator of the package.
-pwr.anova.test(k = 5, n = 7, f = 0.1, sig.level = 0.05, power = NULL)[5] #Power = 0.06537487
-pwr.anova.test(k = 5, n = 7, f = 0.25, sig.level = 0.05, power = NULL)[5] #Power = 0.163053
-pwr.anova.test(k = 5, n = 7, f = 0.4, sig.level = 0.05, power = NULL)[5] #Power = 0.381159
-
-#Determine what kind of effect size I can detect for my experimental design at different power levels. A small effect size is denoted by f = 0.1, medium is f = 0.25, and large is f = 0.4. These values are suggested by the creator of the package.
-pwr.anova.test(k = 5, n = 7, f = NULL, sig.level = 0.05, power = 1)[3] #f = 1e+07
-pwr.anova.test(k = 5, n = 7, f = NULL, sig.level = 0.05, power = 0.95)[3] #f = 0.7887625
-pwr.anova.test(k = 5, n = 7, f = NULL, sig.level = 0.05, power = 0.90)[3] #f = 0.7180425
-pwr.anova.test(k = 5, n = 7, f = NULL, sig.level = 0.05, power = 0.85)[3] #f = 0.6699974
-pwr.anova.test(k = 5, n = 7, f = NULL, sig.level = 0.05, power = 0.80)[3] #f = 0.6316528
