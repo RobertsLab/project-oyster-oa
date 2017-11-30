f543d5a654de58fe8ca775810cfa2587aa431efa
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-11-30 12:06:16 -0800

    increased y-axis label size

--- analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-29-Environmental-Data-Analyses-pH.R
+++ analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-29-Environmental-Data-Analyses-pH.R
@@ -81,13 +81,13 @@ pHBoxplot <- rbind(pHBoxplotCIB, pHBoxplotCIE, pHBoxplotFBB, pHBoxplotFBE, pHBox
 
 #### MAKE BOXPLOT JUST BASED ON SITES ####
 
-jpeg("2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-29-pH-Boxplot-Site-Only.jpeg", height = 1000, width = 2000)
+#jpeg("2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-11-29-pH-Boxplot-Site-Only.jpeg", height = 1000, width = 2000)
 boxplot(pHBoxplot$pH ~ pHBoxplot$Site, ylim = pHRange, main = "pH at Sites", cex.main = 3, cex.axis = 1.5) #Make boxplot based on sites and habitat
 siteANOVA <- aov(pHBoxplot$pH ~ pHBoxplot$Site) #Perform an ANOVA to test for significant differences in pHs between sites
 legend("topright", bty = "n", legend = paste("F =", format(summary(siteANOVA)[[1]][["F value"]][[1]], digits = 4), "p =", format(summary(siteANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add F and p-value from ANOVA
 title(xlab = "Site", cex.lab = 2.5, line = 3.5) #Add x-axis label
 title(ylab = "pH", cex.lab = 2.5, line = 2.5) #Add y-axis label
-dev.off()
+#dev.off()
 
 TukeyHSD(siteANOVA) #Tukey HSD post-hoc test for pH differences between sites. All pairwise differences are significant at 0.05 level.
 
@@ -99,40 +99,40 @@ TukeyHSD(siteANOVA) #Tukey HSD post-hoc test for pH differences between sites. A
 par(mfrow = c(5,2)) #Create multipanel plot with 5 rows and 2 columns
 par(mar = c(0, 0, 10, 0), oma = c(40, 15, 1, 1)) #Remove redundant white space and change outer margins
 
-plot(pHData$WBE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, type = "l", col = "dark grey", main = "Willapa Bay Eelgrass") #Willapa Bay, Eelgrass
+plot(pHData$WBE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, cex.axis = 5, type = "l", col = "dark grey", main = "Willapa Bay Eelgrass") #Willapa Bay, Eelgrass
 abline(h = median(pHData$WBE, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$WBE, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 mtext(side = 2, text = "pH", line = 7, cex = 5, outer = TRUE) #Modify y-axis labels
 
-plot(pHData$WBB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.axis = 5, cex.main = 10, type = "l", col = "dark grey", main = "Willapa Bay Bare") #Willapa Bay, Bare
+plot(pHData$WBB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.main = 10, type = "l", col = "dark grey", main = "Willapa Bay Bare") #Willapa Bay, Bare
 abline(h = median(pHData$WBB, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$WBB, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$FBE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, type = "l", col = "blue", main = "Fidalgo Bay Eelgrass") #Fidgalo Bay, Eelgrass
+plot(pHData$FBE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, cex.axis = 5, type = "l", col = "blue", main = "Fidalgo Bay Eelgrass") #Fidgalo Bay, Eelgrass
 abline(h = median(pHData$FBE, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$FBE, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$FBB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.axis = 5, cex.main = 10, type = "l", col = "blue", main = "Fidalgo Bay Bare") #FBB
+plot(pHData$FBB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.main = 10, type = "l", col = "blue", main = "Fidalgo Bay Bare") #FBB
 abline(h = median(pHData$FBB, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$FBB, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$SKE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, type = "l", col = "green", main = "Skokomish River Delta Eelgrass") #Skokomish River Delta, Eelgrass
+plot(pHData$SKE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, cex.axis = 5, type = "l", col = "green", main = "Skokomish River Delta Eelgrass") #Skokomish River Delta, Eelgrass
 abline(h = median(pHData$SKE, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$SKE, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$SKB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.axis = 5, cex.main = 10, type = "l", col = "green", main = "Skokomish River Delta Bare") #Skokomish River Delta, Bare
+plot(pHData$SKB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, yaxt = "n", cex.main = 10, type = "l", col = "green", main = "Skokomish River Delta Bare") #Skokomish River Delta, Bare
 abline(h = median(pHData$SKB, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$SKB, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$CIE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, type = "l", cex.main = 10, col = "red", main = "Case Inlet Eelgrass") #Case Inlet, Eelgrass
+plot(pHData$CIE, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, type = "l", cex.main = 10, cex.axis = 5, col = "red", main = "Case Inlet Eelgrass") #Case Inlet, Eelgrass
 abline(h = median(pHData$CIE, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$CIE, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$CIB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange,  yaxt = "n", cex.axis = 5, cex.main = 10, type = "l", col = "red", main = "Case Inlet Bare") #Case Inlet, Bare
+plot(pHData$CIB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange,  yaxt = "n", cex.main = 10, type = "l", col = "red", main = "Case Inlet Bare") #Case Inlet, Bare
 abline(h = median(pHData$CIB, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$CIB, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 
-plot(pHData$PGB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, type = "l", col = "magenta", main = "Port Gamble Bay Bare") #Port Gamble Bay, Bare
+plot(pHData$PGB, xlab = "", xaxt = "n", ylab = "", ylim = pHRange, cex.main = 10, cex.axis = 5, type = "l", col = "magenta", main = "Port Gamble Bay Bare") #Port Gamble Bay, Bare
 abline(h = median(pHData$PGB, na.rm = TRUE), lty = 1) #Add line depicting median pH
 abline(h = mean(pHData$PGB, na.rm = TRUE), lty = 2) #Add line depicting mean pH
 axis(side = 1, at = seq(from = 1, to = length(pHData$Date), by = 144*5), lab = pHData$Date[seq(from = 1, to = length(pHData$Date), by = 144*5)], las = 3, cex.axis = 5, line = 2) #Make x-axis
