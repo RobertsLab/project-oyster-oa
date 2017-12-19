2bf87dfdc16c34209e0d2c76bff68da4499039d0
Yaamini Venkataraman <yaaminiv@uw.edu>
2017-12-18 16:09:09 -0800

    made dates recognizable by R

--- analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-12-13-Environmental-Data-Quality-Control/2017-12-13-Environmental-Data-Quality-Control.R
+++ analyses/DNR_SRM_20170902/2017-11-15-Environmental-Data-and-Biomarker-Analyses/2017-12-13-Environmental-Data-Quality-Control/2017-12-13-Environmental-Data-Quality-Control.R
@@ -7,8 +7,9 @@ getwd()
 #### IMPORT AND FORMAT DATA ####
 
 tideData <- read.csv("../../data/DNR/2017-12-13-Tidal-Data-by-Site.csv", header = TRUE) #Import the tide data
-head(tideData)
+head(tideData) #Confirm import
 tideData <- tideData[1:(length(tideData) - 2)] #Remove last two blank columns
+tideData$Date <- as.Date(tideData$Date, format = "%m/%d/%y") #Convert entries to dates
 tideData$DateTime <- paste(tideData$Date, tideData$Time) #Create new DateTime column to easily merge tide and environmental data
 head(tideData) #Confirm changes
 
@@ -16,14 +17,18 @@ pHDOData <- read.csv("../../data/DNR/2017-11-14-Environmental-Data-from-Micah.cs
 head(pHDOData) #Confirm import
 colnames(pHDOData) #View column names
 
-pHData <- pHDOData[,c(1:3, 5, 7, 8, 10, 12)] #Subset only the bare site pH data
+pHData <- pHDOData[,c(2:3, 5, 7, 8, 10, 12)] #Subset only the bare site pH data. Do not include DateTime column, since the formatting is off
 head(pHData) #Confirm subset
-colnames(pHData) <- c("DateTime", "Date", "Time", "WBB-pH", "SKB-pH", "PGB-pH", "CIB-pH", "FBB-pH") #Rename columns
+colnames(pHData) <- c("Date", "Time", "WBB-pH", "SKB-pH", "PGB-pH", "CIB-pH", "FBB-pH") #Rename columns
+pHData$Date <- as.Date(pHData$Date, format = "%m/%d/%y") #Convert entries to dates
+pHData$DateTime <- paste(pHData$Date, pHData$Time) #Create new DateTime column to easily merge tide and environmental data
 head(pHData) #Confirm changes
 
-DOData <- pHDOData[,c(1:3, 23, 25, 27, 29, 31)] #Subset only the bare site DO data
+DOData <- pHDOData[,c(2:3, 23, 25, 27, 29, 31)] #Subset only the bare site DO data
 head(DOData) #Confirm subset
-colnames(DOData) <- c("DateTime", "Date", "Time", "WBB-DO", "SKB-DO", "PGB-DO", "CIB-DO", "FBB-DO") #Rename columns
+colnames(DOData) <- c("Date", "Time", "WBB-DO", "SKB-DO", "PGB-DO", "CIB-DO", "FBB-DO") #Rename columns
+DOData$Date <- as.Date(DOData$Date, format = "%m/%d/%y") #Convert entries to dates
+DOData$DateTime <- paste(DOData$Date, DOData$Time) #Create new DateTime column to easily merge tide and environmental data
 head(DOData) #Confirm changes
 
 salinityData <- read.csv("../../data/DNR/2017-11-25-Calculated-Salinity-Output-from-Micah.csv", header = TRUE, na.strings = "NA") #Import salinity data
@@ -32,6 +37,7 @@ colnames(salinityData) #Get column names
 salinityData <- salinityData[,c(1:2, seq(from = 3, to = 20, by = 2))] #Subset only the salinity information
 head(salinityData) #Confirm subset
 colnames(salinityData) <- c("Date", "Time", "CIB", "CIE", "FBB", "FBE", "PGE", "SKE", "SKB", "WBB", "WBE") #Rename columns
+salinityData$Date <- as.Date(salinityData$Date, format = "%d/%m/%Y") #Convert entries to dates
 salinityData$DateTime <- paste(salinityData$Date, salinityData$Time) #Create new DateTime column to easily merge tide and environmental data
 head(salinityData)
 
