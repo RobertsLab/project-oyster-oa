3687cdd08b7b61f312061dd9f2ffa470d1616dbc
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-03-07 09:35:51 -0800

    plotted pH treatment results for hatch rate

--- analyses/Manchester_ReproductiveOutput_20180214/2018-02-14-Reproductive-Output.R
+++ analyses/Manchester_ReproductiveOutput_20180214/2018-02-14-Reproductive-Output.R
@@ -99,8 +99,6 @@ TukeyHSD(treatmentANOVA) #Significant differences are between Heat Shock and pH 
 
 hatchRate <- read.csv("data/Manchester/2017-07-30-Pacific-Oyster-Larvae/2018-02-14-Hatch-Rate-Data.csv", header = TRUE) #Import hatch rate data
 head(hatchRate) #Confirm import
-#hatchRatepHOnly <- hatchRate[-c(25:26),] #Remove heat shock data
-#tail(hatchRatepHOnly) #Confirm removal
 
 #### VISUALIZE DATA ####
 
@@ -114,6 +112,32 @@ plot(x = hatchRate$Female.Treatment, y = hatchRate$Average.Hatch.Rate, xlab = "T
 
 #jpeg(filename = "analyses/Manchester_ReproductiveOutput_20180214/2018-02-14-Hatch-Rate-by-Male-Treatment.jpeg", width = 1500, height = 1000)
 plot(x = hatchRate$Male.Treatment, y = hatchRate$Average.Hatch.Rate, xlab = "Treatment", ylab = "Hatch Rate", main = "Hatch Rate by Male Treatment", cex.main = 4, cex.axis = 1, cex.lab = 1.4) #Preliminary plot
+#dev.off()
+
+#### VISUALIZE JUST THE PH TREATMENT DATA ####
+
+hatchRatepHOnly <- hatchRate[-c(25:26),] #Remove heat shock data
+tail(hatchRatepHOnly) #Confirm removal
+hatchRatepHOnly$Parental.Treatment <- factor(hatchRatepHOnly$Parental.Treatment) #Make sure residual factors are no longer present
+hatchRatepHOnly$Female.Treatment <- factor(hatchRatepHOnly$Female.Treatment) #Make sure residual factors are no longer present
+hatchRatepHOnly$Male.Treatment <- factor(hatchRatepHOnly$Male.Treatment) #Make sure residual factors are no longer present
+
+hatchRatepHTreatmentANOVA <- aov(Average.Hatch.Rate ~ Parental.Treatment, data = hatchRatepHOnly) #One-way ANOVA by parental treatment
+sqrt(summary(hatchRatepHTreatmentANOVA)[[1]][["F value"]][[1]]) #t = 1.740458
+summary(hatchRatepHTreatmentANOVA)[[1]][["Pr(>F)"]][[1]] #p = 0.0533657
+
+#jpeg(filename = "analyses/Manchester_ReproductiveOutput_20180214/2018-03-07-Hatch-Rate-by-pH-Treatment-All-Groups.jpeg", width = 1500, height = 1000)
+plot(x = hatchRatepHOnly$Parental.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, xlab = "Treatment", ylab = "Hatch Rate", main = "Hatch Rate by Treatment", cex.main = 4, cex.axis = 1, cex.lab = 1.4) #Preliminary plot
+legend("topright", bty = "n", legend = paste("t =", format(sqrt(summary(hatchRatepHTreatmentANOVA)[[1]][["F value"]][[1]]), digits = 4), "p =", format(summary(hatchRatepHTreatmentANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add t and p-value
+#dev.off()
+
+hatchRatepHTreatmentFemaleANOVA <- aov(Average.Hatch.Rate ~ Female.Treatment, data = hatchRatepHOnly) #One-way ANOVA by female treatment
+sqrt(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["F value"]][[1]]) #t = 3.161651
+summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["Pr(>F)"]][[1]] #p = 0.004522199
+
+#jpeg(filename = "analyses/Manchester_ReproductiveOutput_20180214/2018-03-07-Hatch-Rate-by-pH-Treatment-Female-Groups.jpeg", width = 1500, height = 1000)
+plot(x = hatchRatepHOnly$Female.Treatment, y = hatchRatepHOnly$Average.Hatch.Rate, xlab = "Treatment", ylab = "Hatch Rate", main = "Hatch Rate by Female Treatment", cex.main = 4, cex.axis = 1, cex.lab = 1.4) #Preliminary plot
+legend("topright", bty = "n", legend = paste("t =", format(sqrt(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["F value"]][[1]]), digits = 4), "p =", format(summary(hatchRatepHTreatmentFemaleANOVA)[[1]][["Pr(>F)"]][[1]], digits = 4))) #Add t and p-value
 #dev.off()
 
 #### ANOVA ####
