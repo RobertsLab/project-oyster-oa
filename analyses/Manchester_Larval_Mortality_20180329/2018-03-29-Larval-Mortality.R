7118515097c4599988ff123361491a0e44c77c71
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-03-29 15:17:16 -0700

    created new script for larval mortality analysis

---
+++ analyses/Manchester_Larval_Mortality_20180329/2018-03-29-Larval-Mortality.R
@@ -0,0 +1,6 @@
+#In this script, I'll plot my larval abundance data with error bars. Then, I'll see if analysis is worth it
+
+#### SET WORKING DIRECTORY ####
+
+getwd()
+setwd("../../") #Set working directory as repository
