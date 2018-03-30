cedcb3d01724b83e816a70193f34cf2a0e96d621
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-03-30 13:00:04 -0700

    added color designations to data frame

--- analyses/Manchester_Larval_Mortality_20180329/2018-03-29-Larval-Mortality.R
+++ analyses/Manchester_Larval_Mortality_20180329/2018-03-29-Larval-Mortality.R
@@ -36,3 +36,27 @@ for(i in 1:nBuckets){
 } #Create and populate a new dataframe
 head(larvalCountsBuckets) #Confirm changes
 
+attach(larvalCountsBuckets)
+larvalCountsBuckets <- larvalCountsBuckets[order(Bucket),] #Reorder so buckets are sorted alphabetically
+head(larvalCountsBuckets) #Confirm sorting
+tail(larvalCountsBuckets) #Confirm sorting
+detach(larvalCountsBuckets)
+
+larvalCountsBuckets$Colors <- c(rep("red", times = 50),
+                                rep("pink", times = 50),
+                                rep("green", times = 50),
+                                rep("blue", times = 50),
+                                rep("orange", times = 40)) #Create a column to assign colors
+#Buckets 1-5 are FLxML: red
+#Buckets 6-10 are FLxMA: pink
+#Buckets 11-15 are FAxML: green
+#Buckets 16-20 are FAxMA: blue
+#Buckets 21-24 are HSxHS: orange
+head(larvalCountsBuckets) #Confirm column addition
+
+#### PLOT DATA ####
+
+plot(x = larvalCountsBuckets$Date, y = larvalCountsBuckets$Count, type = "l")
+
+
+?plot
