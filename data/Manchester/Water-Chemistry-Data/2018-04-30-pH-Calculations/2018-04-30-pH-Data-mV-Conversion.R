9fd7554155a5176185d7cbd1036126feb20903ee
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-04-30 14:00:21 -0700

    isolated grab sample data by date

---
+++ data/Manchester/Water-Chemistry-Data/2018-04-30-pH-Calculations/2018-04-30-pH-Data-mV-Conversion.R
@@ -0,0 +1,73 @@
+#A code to calibrate Tris calibrate your pH sensor on the total scale and calculate pH from mV and Temperature
+#pH data is exported as a csv file with the name "Data Today's Date" into your working directory
+#Modified by Nyssa Silbiger 6/25/14
+#Edited 1/7/2016
+#Edited by Yaamini Venkataraman on 4/30/2018
+
+#### SET WORKING DIRECTORY ####
+main<-setwd(getwd())
+
+#### LOAD LIBRARIES AND SET FUNCTIONALITY ####
+library(seacarb)
+rm(list=ls())
+
+#### LOAD DATA FILES (ONLY CHANGE THESE TWO LINES OF CODE) ####
+filename.calib <- "CDMNightCalibration.csv" #Calibration file
+filename.data <- "CDMNightpHData.csv" #Data file
+
+#### READ IN CALIBRATION DATA ####
+z<-read.table(filename.calib, header = T, sep=",", na.string="NA") 
+#mVTris is milivolts of the tris calibration
+#TTris is temperature of the tris calibration 
+#there needs to be at least three points of Tris values at different temperature
+
+attach(z) #this lets me search the variable names within "z"
+
+#load your own data
+Data <- read.table(filename.data, header=T, sep=",", na.string="NA") #make sure to change "your data"
+#mV is milivolts from your data
+#Tin is temperature from your data (measured at the time of collection)
+
+#### RUN THE CALIBRATION ####
+#fit a linear model between temperature and mV
+mVTris_t<-lm(mVTris~TTris)
+
+print(summary(mVTris_t))
+
+#plot temperature vs millivolt -- make sure it looks like a straight line
+plot(TTris, mVTris, xlab="Temperature", ylab="mV")
+abline(mVTris_t)
+
+#### FIT PH FOR DATA ####
+attach(Data)
+#variables
+#Tin is your temperature in situ
+#mV is your milivolt
+
+#constants
+R<- 8.31451 #gas constant
+Far<-96485.309 #Faraday constant
+
+#Tin<-T#Temp measured in situ
+
+#calculate the pH of the tris (Dickson A. G., Sabine C. L. and Christian J. R., SOP 6a)
+mvTris<-Tin*mVTris_t$coefficients[2]+mVTris_t$coefficients[1]
+
+STris<-34.5 #salinity of the Tris
+
+phTris<- (11911.08-18.2499*STris-0.039336*STris^2)*(1/(Tin+273.15))-366.27059+ 0.53993607*STris+0.00016329*STris^2+(64.52243-0.084041*STris)*log(Tin+273.15)-0.11149858*(Tin+273.15)
+
+# calculate pherror
+Tris<-lm(phTris~Tin) #Linear model of temperate by pH of tris
+TrisCalc<-25*Tris$coefficients[2]+Tris$coefficients[1] #calculate the pH at 25 deg C
+pHError<-((TrisCalc-8.0835)/8.0835)*100 #percent error of your probe measurement
+
+
+#calculate the pH of your samples
+pH<-phTris+(mvTris/1000-mV/1000)/(R*(Data$Tin+273.15)*log(10)/Far)
+
+Data<-cbind(Data,pH)
+
+#setwd(file.path("Data/pH"))
+date<-strsplit(as.character(Sys.time()),split=" ")[[1]]
+write.table(Data,paste("Data",date[1],".csv"),sep=",", col.names = NA)
