396d49f0e619e3a7783cc195005b416c95054e85
Yaamini Venkataraman <yaaminiv@uw.edu>
2018-12-11 00:31:21 -0800

    added PS designation to map

--- images/DNR/2018-05-23-Site-Map/2018-05-23-Outplant-Site-Map-Code.R
+++ images/DNR/2018-05-23-Site-Map/2018-05-23-Outplant-Site-Map-Code.R
@@ -4,8 +4,6 @@
 
 rm(list = ls()) #Remove list
 
-?svg
-
 #Install packages
 install.packages("maps") #Basic mapping functions and some data
 install.packages("mapdata") #Some additional HiRes data
@@ -28,21 +26,22 @@ locationCords <- locationCords[,1:4] #Only keep columns with data
 locationCords <- locationCords[order(locationCords$Latitude),] #Reorder location coordinates by latittude
 head(locationCords) #Confirm changes
 
-#### CREATE BASE MAP ####
-#svg(filename = "images/DNR/2018-05-23-Site-Map/2018-05-23-Outplant-Site-svg") #Create a new file to save map
-
-data(nepacLLhigh) #Load set of polygons for the NE Pacific Ocean in high resolution from PBSmapping
-plotMap(nepacLLhigh, xlim = c(-125, -121.9), ylim = c(46, 48.9), col = "snow1", bg = "slategray1", xaxt = "n", yaxt = "n", xlab = "", ylab = "", ann = FALSE) #Create a map with high resolution NE Pacific Ocean data. Remove axes since those will be manually added
-
-#### MODIFY BASE MAP ####
-axis(side = 1, at = c(-124.5, -124, -123.5, -123, -122.5, -122), labels=c("124.5°W", "124°W", "123.5°W", "123°W", "122.5°W", "122°W"), tick = TRUE, col.axis = "grey20") #Add longitude axis
-axis(side = 2, at = c(46.5, 47, 47.5, 48, 48.5), labels=c("46.5ºN", "47°N", "47.5°N", "48°N", "48.5°N"), tick = TRUE, col.axis = "grey20") #Add latitude axis
-
 textCoordinates <- locationCords #Duplicate dataframe
 textCoordinates$Latitude[5] <- 48.4 #Move FB text down so it's easier to read
 head(textCoordinates) #Confirm changes
 
-symbols(x = locationCords$Longitude, y = locationCords$Latitude, circles = c(rep(1, times = 5)), add = TRUE, inches = 0.05, bg = "grey30") #Add points to map
-text(x = textCoordinates$Longitude, y = textCoordinates$Latitude, labels = locationCords$Abbreviation, pos = c(2, 2, 2, 1, 4), col = "black", cex = 1.8) #Add site abbreviations as labels
+#### CREATE MAP ####
+#pdf("images/DNR/2018-05-23-Site-Map/2018-05-23-Outplant-Site.pdf", width = 8.5, height = 11) #Create a new file to save map
+
+data(nepacLLhigh) #Load set of polygons for the NE Pacific Ocean in high resolution from PBSmapping
+plotMap(nepacLLhigh, xlim = c(-125, -121.9), ylim = c(46, 48.9), col = "grey80", bg = "white", xaxt = "n", yaxt = "n", xlab = "", ylab = "", ann = FALSE) #Create a map with high resolution NE Pacific Ocean data. Remove axes since those will be manually added
+
+axis(side = 1, at = c(-124.5, -124, -123.5, -123, -122.5, -122), labels=c("124.5°W", "124°W", "123.5°W", "123°W", "122.5°W", "122°W"), tick = TRUE, col.axis = "grey20") #Add longitude axis
+axis(side = 2, at = c(46.5, 47, 47.5, 48, 48.5), labels=c("46.5ºN", "47°N", "47.5°N", "48°N", "48.5°N"), tick = TRUE, col.axis = "grey20") #Add latitude axis
+
+rect(xleft = -123.3, xright = -122.1, ytop = 48.6, ybottom = 47, border = "grey20") #Add a box around Puget Sound
+
+symbols(x = locationCords$Longitude, y = locationCords$Latitude, circles = c(rep(1, times = 5)), add = TRUE, inches = 0.05, bg = c("#EB8B0C", "#017A74", "#00A9BD", "#440D82", "#38001C")) #Add points to map
+text(x = textCoordinates$Longitude, y = textCoordinates$Latitude, labels = locationCords$Abbreviation, pos = c(2, 2, 2, 1, 4), col = c("#EB8B0C", "#017A74", "#00A9BD", "#440D82", "#38001C"), cex = 1.8) #Add site abbreviations as labels
 
 #dev.off() #Turn off plotting device
